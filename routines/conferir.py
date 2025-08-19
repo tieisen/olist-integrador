fa1218ad4f0bf88712a30d3b86dccbdd8a641c12
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-08-19 16:16:23 -0300

    update

--- routines/conferir.py
+++ routines/conferir.py
@@ -1,6 +1,8 @@
 from src.integrador.pedido import Pedido
 import asyncio
+import nest_asyncio
 
 if __name__=="__main__":
+    nest_asyncio.apply()
     pedido = Pedido()
     asyncio.run(pedido.conferir())    
