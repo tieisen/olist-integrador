2da6d0a41b8db504e2fde6ecf2842ab231b51958
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-08-21 10:55:13 -0300

    incluida busca das separacoes no recebimento dos pedidos

--- routines/pedidos_buscar.py
+++ routines/pedidos_buscar.py
@@ -1,9 +1,12 @@
 from src.integrador.pedido import Pedido
+from src.integrador.separacao import Separacao
 import asyncio
 
 # ROTINA A SER EXECUTADA DIARIAMENTE, A CADA 30 MINUTOS, ENTRE ÀS 7H E 11H
 
 if __name__=="__main__":    
     pedido = Pedido()
+    separacao = Separacao()
     asyncio.run(pedido.receber())
     asyncio.run(pedido.validar_cancelamentos())
+    asyncio.run(separacao.receber())    
