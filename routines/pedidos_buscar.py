11bfc23a77fa9c0f07be7a1b2c43cdfcbcbfef0f
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-08-28 16:23:21 -0300

    rotina de separacao removida

--- routines/pedidos_buscar.py
+++ routines/pedidos_buscar.py
@@ -1,12 +1,7 @@
 import asyncio
 from src.integrador.pedido import Pedido
-from src.integrador.separacao import Separacao
-
-# ROTINA A SER EXECUTADA DIARIAMENTE, A CADA 30 MINUTOS
 
 if __name__=="__main__":    
     pedido = Pedido()
-    separacao = Separacao()
     asyncio.run(pedido.receber())
     asyncio.run(pedido.validar_cancelamentos())
-    asyncio.run(separacao.receber())    
