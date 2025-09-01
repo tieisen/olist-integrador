c0b4e4a2ae92ba3cc5528e2da01ba8f1182509ed
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-01 14:42:49 -0300

    ajustado para modo assincrono

--- database/crud/log_produto.py
+++ database/crud/log_produto.py
@@ -1,59 +1,40 @@
-from database.database import SessionLocal
+from database.database import AsyncSessionLocal
 from database.models import LogProduto
-from database.models import Log
-from datetime import datetime
+from src.utils.log import Log
+import os
+import logging
+from dotenv import load_dotenv
 
-def criar(log_id:int=None,codprod:int=None,idprod:int=None,campo:str=None,valor_old:str=None,valor_new:str=None,sucesso:bool=True,obs:str=None):
-    session = SessionLocal()
-    novo_log = LogProduto(dh_atualizacao=datetime.now(),
-                          log_id=log_id,
-                          codprod=codprod,
-                          idprod=idprod,
-                          campo=campo,
-                          sucesso=sucesso,
-                          valor_old=str(valor_old),
-                          valor_new=str(valor_new),
-                          obs=obs)
-    session.add(novo_log)
-    session.commit()
-    session.refresh(novo_log)
-    session.close()
-    return True
+load_dotenv('keys/.env')
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename=Log().buscar_path(),
+                    encoding='utf-8',
+                    format=os.getenv('LOGGER_FORMAT'),
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO)
 
-def buscar_todos_codprod(codprod: int):
-    session = SessionLocal()
-    log = session.query(LogProduto).filter(LogProduto.codprod == codprod).all()
-    session.close()
-    return log
+async def criar(log_id:int,codprod:int,idprod:int,campo:str,valor_old:str=None,valor_new:str=None,sucesso:bool=True,obs:str=None):
+    async with AsyncSessionLocal() as session:
+        novo_log = LogProduto(log_id=log_id,
+                              codprod=codprod,
+                              idprod=idprod,
+                              campo=campo,
+                              sucesso=sucesso,
+                              valor_old=str(valor_old),
+                              valor_new=str(valor_new),
+                              obs=obs)
+        session.add(novo_log)
+        session.commit()
+        session.refresh(novo_log)
+        return True
 
-def buscar_ultimo_codprod(codprod: int):
-    session = SessionLocal()
-    ultimo_log = session.query(LogProduto).filter(Log.contexto == 'produto', LogProduto.codprod == codprod).order_by(LogProduto.log_id.desc()).first()
-    if not ultimo_log:
-        session.close()
-        return False
-    log = session.query(LogProduto).filter(LogProduto.log_id == ultimo_log.id, LogProduto.codprod == codprod).all()
-    session.close()
-    return log
+async def buscar_status_false(log_id: int):
+    async with AsyncSessionLocal() as session:
+        log = await session.query(LogProduto).filter(LogProduto.log_id == log_id,
+                                                     LogProduto.sucesso != 1).first()
+        return log
 
-def buscar_ultimo():
-    session = SessionLocal()
-    ultimo_log = session.query(LogProduto).filter(Log.contexto == 'produto').order_by(LogProduto.log_id.desc()).first()
-    if not ultimo_log:
-        session.close()
-        return False
-    log = session.query(LogProduto).filter(LogProduto.log_id == ultimo_log.id).all()
-    session.close()
-    return log
-
-def buscar_status_false(log_id: int):
-    session = SessionLocal()
-    log = session.query(LogProduto).filter(LogProduto.log_id == log_id, LogProduto.sucesso != 1).first()
-    session.close()
-    return log
-
-def buscar_id(log_id: int):
-    session = SessionLocal()
-    log = session.query(LogProduto).filter(LogProduto.log_id == log_id).all()
-    session.close()
-    return log
+async def buscar_id(log_id: int):
+    async with AsyncSessionLocal() as session:
+        log = await session.query(LogProduto).filter(LogProduto.log_id == log_id).all()
+        return log
