e2be644bcf6d65d76a70722bb3b44c8b33282dd5
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-01 14:42:43 -0300

    ajustado para modo assincrono

--- database/crud/log_pedido.py
+++ database/crud/log_pedido.py
@@ -1,46 +1,36 @@
-from database.database import SessionLocal
+from database.database import AsyncSessionLocal
 from database.models import LogPedido
-from datetime import datetime
+from src.utils.log import Log
+import os
+import logging
+from dotenv import load_dotenv
 
-def criar(log_id:int,id_loja:int,id_pedido:int,pedido_ecommerce:str,evento:str='I',nunota_pedido:int=0,status:bool=True,obs:str=None,nunota_nota:int=0, id_nota:int=0):
-    session = SessionLocal()
-    novo_log = LogPedido(log_id=log_id,
-                         dh_atualizacao=datetime.now(),
-                         id_loja=id_loja,
-                         id_pedido=id_pedido,
-                         pedido_ecommerce=pedido_ecommerce,
-                         evento=evento,
-                         nunota_nota=nunota_nota,
-                         id_nota=id_nota,
-                         nunota_pedido=nunota_pedido,
-                         status=status,
-                         obs=obs)
-    session.add(novo_log)
-    session.commit()
-    session.refresh(novo_log)
-    session.close()
-    return True
+load_dotenv('keys/.env')
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename=Log().buscar_path(),
+                    encoding='utf-8',
+                    format=os.getenv('LOGGER_FORMAT'),
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO)
 
-def buscar_id(log_id: int):
-    session = SessionLocal()
-    log = session.query(LogPedido).filter(LogPedido.log_id == log_id).all()
-    session.close()
-    return log
+async def criar(log_id:int,pedido_id:int,evento:str,status:bool=True,obs:str=None):
+    async with AsyncSessionLocal() as session:
+        novo_log = LogPedido(log_id=log_id,
+                             pedido_id=pedido_id,
+                             evento=evento,
+                             status=status,
+                             obs=obs)
+        session.add(novo_log)
+        session.commit()
+        return True
 
-def buscar_id_pedido(id_pedido: int):
-    session = SessionLocal()
-    log = session.query(LogPedido).filter(LogPedido.id_pedido == id_pedido).first()
-    session.close()
-    return log
+async def buscar_id(log_id: int):
+    async with AsyncSessionLocal() as session:
+        log = await session.query(LogPedido).filter(LogPedido.log_id == log_id).all()
+        return log
 
-def buscar_nunota_pedido(nunota_pedido: int):
-    session = SessionLocal()
-    log = session.query(LogPedido).filter(LogPedido.nunota_pedido == nunota_pedido).first()
-    session.close()
-    return log
-
-def buscar_status_false(log_id: int):
-    session = SessionLocal()
-    log = session.query(LogPedido).filter(LogPedido.log_id == log_id, LogPedido.status.is_(False)).first()
-    session.close()
-    return log
+async def buscar_status_false(log_id: int):
+    async with AsyncSessionLocal() as session:
+        log = await session.query(LogPedido).filter(LogPedido.log_id == log_id,
+                                                    LogPedido.status.is_(False)).first()
+        return log
