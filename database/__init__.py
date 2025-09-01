9fa8db2ebbd935f7d246fdbac5ee11e10087c771
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-01 09:03:56 -0300

    inicializacao do banco

---
+++ database/__init__.py
@@ -0,0 +1,9 @@
+from database.database import *
+import asyncio
+
+async def criar_tabelas():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+if __name__ == "__main__":
+    asyncio.run(criar_tabelas())
