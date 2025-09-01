d7cdbf4f44f15d53a735c9f0595dfd84bcf84148
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-01 14:42:38 -0300

    ajustado para modo assincrono

--- database/crud/log_estoque.py
+++ database/crud/log_estoque.py
@@ -1,36 +1,39 @@
-from database.database import SessionLocal
+from database.database import AsyncSessionLocal
 from database.models import LogEstoque
-from datetime import datetime
+from src.utils.log import Log
+import os
+import logging
+from dotenv import load_dotenv
 
-def criar(log_id:int,codprod:int,idprod:int,status_estoque:bool=True,qtdmov:int=0,obs:str=None):
-    session = SessionLocal()
-    novo_log = LogEstoque(log_id=log_id,
-                          dh_atualizacao=datetime.now(),
-                          codprod=codprod,
-                          idprod=idprod,
-                          qtdmov=qtdmov,
-                          status_estoque=status_estoque,
-                          obs=obs)
-    session.add(novo_log)
-    session.commit()
-    session.refresh(novo_log)
-    session.close()
-    return True
+load_dotenv('keys/.env')
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename=Log().buscar_path(),
+                    encoding='utf-8',
+                    format=os.getenv('LOGGER_FORMAT'),
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO)
 
-def buscar_codprod(codprod: int):
-    session = SessionLocal()
-    log = session.query(LogEstoque).filter(LogEstoque.codprod == codprod).order_by(LogEstoque.dh_atualizacao.desc()).all()
-    session.close()
-    return log
+async def criar(log_id:int,codprod:int,idprod:int,qtdmov:int=0,status_estoque:bool=True,status_lote:bool=None,obs:str=None):
+    async with AsyncSessionLocal() as session:
+        novo_log = LogEstoque(log_id=log_id,
+                              codprod=codprod,
+                              idprod=idprod,
+                              qtdmov=qtdmov,
+                              status_estoque=status_estoque,
+                              status_lote=status_lote,                              
+                              obs=obs)
+        session.add(novo_log)
+        await session.commit()
+        await session.refresh(novo_log)
+        return novo_log.id
 
-def buscar_status_false(log_id: int):
-    session = SessionLocal()
-    log = session.query(LogEstoque).filter(LogEstoque.log_id == log_id, LogEstoque.status_estoque != 1).first()
-    session.close()
-    return log
+async def buscar_status_false(log_id: int):
+    async with AsyncSessionLocal() as session:
+        log = await session.query(LogEstoque).filter(LogEstoque.log_id == log_id,
+                                                     LogEstoque.status_estoque != 1).first()
+        return log
 
-def buscar_id(log_id: int):
-    session = SessionLocal()
-    log = session.query(LogEstoque).filter(LogEstoque.log_id == log_id).all()
-    session.close()
-    return log
+async def buscar_id(log_id: int):
+    async with AsyncSessionLocal() as session:
+        log = await session.query(LogEstoque).filter(LogEstoque.log_id == log_id).all()
+        return log
