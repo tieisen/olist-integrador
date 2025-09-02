48a5640dd316ca5f54727da0a6e1eccf8304083f
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-02 11:11:49 -0300

    excluir

--- src/sankhya/pedido.py
+++ src/sankhya/pedido.py
@@ -413,6 +413,41 @@ class Pedido:
             logger.error("Erro ao faturar pedido. Nunota %s. %s",nunota,res.text)
             return False, None
 
+    async def excluir(self, nunota:int) -> bool:
+        
+        url = os.getenv('SANKHYA_URL_DELETE')
+        if not url:
+            print(f"Erro relacionado à url. {url}")
+            logger.error("Erro relacionado à url. %s",url)
+            return False, None
+        
+        try:
+            token = self.con.get_token()
+        except Exception as e:
+            logger.error("Erro relacionado ao token de acesso. %s",e)
+            return False, None
+
+        body = {
+            "serviceName": "DatasetSP.removeRecord",
+            "requestBody": {
+                "entityName": "CabecalhoNota",
+                "standAlone": False,
+                "pks": [{"NUNOTA": f"{nunota}"}]
+            }
+        }
+
+        res = requests.get(
+            url=url,
+            headers={ 'Authorization': token },
+            json=body)
+        
+        if res.status_code in (200,201) and res.json().get('status') in ('0','1'):
+            return True
+        else:
+            logger.error("Erro ao excluir pedido. %s",res.json())
+            print(f"Erro ao excluir pedido. {res.json()}")
+            return False
+
 class Itens(Pedido):
     def __init__(self):
         self.formatter = Formatter()
