78b6e45a5aa21543b12b1985137dc04fb392fa1d
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-02 14:09:00 -0300

    update

--- database/database.py
+++ database/database.py
@@ -2,10 +2,10 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
-
-load_dotenv()
+load_dotenv('keys/.env',encoding='latin-1')
 
 DATABASE_URL = os.getenv("DBTEST_MULTI")
+print(DATABASE_URL)
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
