68d62e250259d7aedd876a2d55fd133d74869227
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-04 10:42:17 -0300

    ajustado para modo assincrono

--- database/crud/sankhya.py
+++ database/crud/sankhya.py
@@ -1,6 +1,7 @@
 from database.database import AsyncSessionLocal
 from database.models import Sankhya
 from datetime import datetime, timedelta
+from sqlalchemy.future import select
 from src.utils.log import Log
 import os
 import logging
@@ -14,12 +15,16 @@ logging.basicConfig(filename=Log().buscar_path(),
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO)
 
-async def criar(empresa_id:int, token_criptografado:str,dh_expiracao_token:str):
+async def criar(
+        empresa_id:int,
+        token_criptografado:str,
+        dh_expiracao_token:str
+    ):
     async with AsyncSessionLocal() as session:    
         try:
             novo_token = Sankhya(empresa_id=empresa_id,
-                               token_criptografado=token_criptografado,
-                               dh_expiracao_token=dh_expiracao_token)
+                                 token_criptografado=token_criptografado,
+                                 dh_expiracao_token=dh_expiracao_token)
             session.add(novo_token)
             await session.commit()
             await session.refresh(novo_token)
@@ -30,13 +35,20 @@ async def criar(empresa_id:int, token_criptografado:str,dh_expiracao_token:str):
 
 async def buscar(empresa_id:int):
     async with AsyncSessionLocal() as session:
-        token = await session.query(Sankhya).filter(Sankhya.empresa_id == empresa_id).order_by(Sankhya.id.desc()).first()
-        return token
+        result = await session.execute(
+            select(Sankhya).where(Sankhya.empresa_id == empresa_id).order_by(Sankhya.id.desc()).fetch(1)
+        )
+        token = result.scalar_one_or_none()
+        return token.token_criptografado if token else None
 
 async def excluir(id:int):
     async with AsyncSessionLocal() as session:
-        token = await session.query(Sankhya).filter(Sankhya.id == id).first()
+        result = await session.execute(
+            select(Sankhya).where(Sankhya.id == id)
+        )
+        token = result.scalar_one_or_none()
         if not token:
+            print("Token não encontrado")
             return False
         try:
             await session.delete(token)
@@ -46,11 +58,14 @@ async def excluir(id:int):
             logger.error("Erro ao excluir token do banco de dados: %s", e)
             return False
 
-async def excluir_cache(empresa_id:int):
+async def excluir_cache(dias:int=7):
     async with AsyncSessionLocal() as session:
-        tokens = await session.query(Sankhya).filter(Sankhya.empresa_id == empresa_id,
-                                                     Sankhya.dh_solicitacao < (datetime.now()-timedelta(days=7))).all()
+        result = await session.execute(
+            select(Sankhya).where(Sankhya.dh_solicitacao < (datetime.now()-timedelta(days=dias)))
+        )
+        tokens = result.scalars().all()
         if not tokens:
+            print("Tokens não encontrados")
             return False
         try:
             for token in tokens:
