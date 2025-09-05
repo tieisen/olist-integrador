c06a2552e6cbfc70401a7faf2a5a474dd4376d31
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-05 10:43:20 -0300

    ajuste buscar falhas

--- database/crud/log_produto.py
+++ database/crud/log_produto.py
@@ -1,6 +1,7 @@
 from database.database import AsyncSessionLocal
 from database.models import LogProduto
 from src.utils.log import Log
+from sqlalchemy.future import select
 import os
 import logging
 from dotenv import load_dotenv
@@ -13,8 +14,18 @@ logging.basicConfig(filename=Log().buscar_path(),
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO)
 
-async def criar(log_id:int,codprod:int,idprod:int,campo:str,valor_old:str=None,valor_new:str=None,sucesso:bool=True,obs:str=None):
+async def criar(
+        log_id:int,
+        codprod:int,
+        idprod:int,
+        campo:str,        
+        valor_old:str=None,
+        valor_new:str=None,
+        sucesso:bool=True,
+        obs:str=None,
+        produto_id:int=None):
     async with AsyncSessionLocal() as session:
+
         novo_log = LogProduto(log_id=log_id,
                               codprod=codprod,
                               idprod=idprod,
@@ -22,19 +33,28 @@ async def criar(log_id:int,codprod:int,idprod:int,campo:str,valor_old:str=None,v
                               sucesso=sucesso,
                               valor_old=str(valor_old),
                               valor_new=str(valor_new),
-                              obs=obs)
+                              obs=obs,
+                              produto_id=produto_id)
         session.add(novo_log)
         await session.commit()
         await session.refresh(novo_log)
         return True
 
-async def buscar_status_false(log_id: int):
+async def buscar_falhas(log_id: int):
     async with AsyncSessionLocal() as session:
-        log = await session.query(LogProduto).filter(LogProduto.log_id == log_id,
-                                                     LogProduto.sucesso != 1).first()
+        result = await session.execute(
+            select(LogProduto)
+            .where(LogProduto.log_id == log_id,
+                   LogProduto.sucesso.is_(False))
+        )
+        log = result.scalars().all()
         return log
 
 async def buscar_id(log_id: int):
     async with AsyncSessionLocal() as session:
-        log = await session.query(LogProduto).filter(LogProduto.log_id == log_id).all()
+        result = await session.execute(
+            select(LogProduto)
+            .where(LogProduto.log_id == log_id)
+        )
+        log = result.scalars().all()
         return log
