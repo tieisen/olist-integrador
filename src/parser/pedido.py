b509b3606a469003f4a4e8f31a8f4a92fb6fd97f
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-11 11:39:23 -0300

    parser.pedido: decorador para dados da empresa

--- src/parser/pedido.py
+++ src/parser/pedido.py
@@ -1,9 +1,12 @@
 import os
 import re
 import logging
+import asyncio
 from datetime import datetime
 from dotenv import load_dotenv
+
 from src.utils.log import Log
+from src.utils.decorador.empresa import ensure_dados_empresa
 
 load_dotenv('keys/.env')
 logger = logging.getLogger(__name__)
@@ -15,21 +18,23 @@ logging.basicConfig(filename=Log().buscar_path(),
 
 class Pedido:
 
-    def __init__(self):
-        self.codemp = int(os.getenv('SANKHYA_CODEMP'))
-        self.codnat = int(os.getenv('SANKHYA_CODNAT'))
-        self.codparc = int(os.getenv('SANKHYA_CODPARC'))
-        self.codtipoper = int(os.getenv('SANKHYA_CODTIPOPER'))
-        self.codtipvenda = int(os.getenv('SANKHYA_CODTIPVENDA'))
-        self.codvend = int(os.getenv('SANKHYA_CODVEND'))
-        self.codlocalorig = int(os.getenv('SANKHYA_CODLOCAL'))
-        
-    def to_sankhya(self, dados_olist:dict=None, dados_cidade:list=None) -> tuple[dict,list]:
+    def __init__(self, codemp:int):
+        self.codemp = codemp
+        self.dados_empresa = None
+
+    @ensure_dados_empresa
+    async def buscar_dados_empresa(self):
+        return self.dados_empresa
+
+    def to_sankhya(
+            self,
+            dados_olist:dict,
+            dados_cidade:list
+        ) -> tuple[dict,list]:
         dados_sankhya = {}
         lista_itens = []
 
-        if not all([dados_olist,dados_cidade]):
-            return dados_sankhya, lista_itens
+        asyncio.run(self.buscar_dados_empresa())
 
         dados_sankhya['AD_MKP_CODPED'] = {"$":str(dados_olist['ecommerce'].get('numeroPedidoEcommerce'))}
         dados_sankhya['AD_MKP_DESTINO'] = {"$": dados_cidade.get('codcid')} # ID DA CIDADE NO SAKNHYA
@@ -43,11 +48,11 @@ class Pedido:
         dados_sankhya['CODCIDDESTINO'] = {"$":dados_cidade.get('codcid')}
         dados_sankhya['CODUFDESTINO'] = {"$":dados_cidade.get('uf')}
         dados_sankhya['CODEMP'] = {"$":self.codemp}
-        dados_sankhya['CODNAT'] = {"$":self.codnat}
-        dados_sankhya['CODPARC'] = {"$":self.codparc}
-        dados_sankhya['CODTIPOPER'] = {"$":self.codtipoper}
-        dados_sankhya['CODTIPVENDA'] = {"$":self.codtipvenda}
-        dados_sankhya['CODVEND'] = {"$":self.codvend}
+        dados_sankhya['CODNAT'] = {"$":self.dados_empresa.get('snk_codnat')}
+        dados_sankhya['CODPARC'] = {"$":self.dados_empresa.get('snk_codparc')}
+        dados_sankhya['CODTIPOPER'] = {"$":self.dados_empresa.get('snk_top_pedido')}
+        dados_sankhya['CODTIPVENDA'] = {"$":self.dados_empresa.get('snk_codtipvenda')}
+        dados_sankhya['CODVEND'] = {"$":self.dados_empresa.get('snk_codvend')}
         data_negociacao = datetime.strptime(dados_olist.get('data'),'%Y-%m-%d').strftime('%d/%m/%Y')
         dados_sankhya['DTNEG'] = {"$":data_negociacao}
         dados_sankhya['NUNOTA'] = {},
@@ -66,33 +71,38 @@ class Pedido:
             dados_item['PERCDESC'] = {"$":'0'}
             dados_item['IGNOREDESCPROMOQTD'] = {"$": "True"}
             dados_item['CODVOL'] = {"$":"UN"}
-            dados_item['CODLOCALORIG'] = {"$":self.codlocalorig}
+            dados_item['CODLOCALORIG'] = {"$":self.dados_empresa.get('snk_codlocal_venda')}
             lista_itens.append(dados_item)
             
         return dados_sankhya, lista_itens
     
-    def to_sankhya_lote(self, lista_pedidos:list, lista_itens:list) -> tuple[dict,list,int]:
+    def to_sankhya_lote(
+            self,
+            lista_pedidos:list,
+            lista_itens:list
+        ) -> tuple[dict,list,int]:
 
         def formatar_pedidos(lista_pedidos):
             linhas = [f"- {pedido['numero']}/{pedido['codigo']}" for pedido in lista_pedidos]
             return f"Referente à {len(lista_pedidos)} pedidos:\n" + "\n".join(linhas)        
 
         dados_cabecalho = {}
-        dados_itens = []       
+        dados_itens = []
 
         data_negociacao = datetime.now().strftime('%d/%m/%Y')
         observacao = formatar_pedidos(lista_pedidos)
         origem = lista_pedidos[0].get('origem')
+        asyncio.run(self.buscar_dados_empresa())        
         
         dados_cabecalho['AD_MKP_ORIGEM'] = {"$":origem}
         dados_cabecalho['CIF_FOB'] = {"$":'C'}
         dados_cabecalho['CODCENCUS'] = {"$":'0'}
         dados_cabecalho['CODEMP'] = {"$":self.codemp}
-        dados_cabecalho['CODNAT'] = {"$":self.codnat}
-        dados_cabecalho['CODPARC'] = {"$":self.codparc}
-        dados_cabecalho['CODTIPOPER'] = {"$":self.codtipoper}
-        dados_cabecalho['CODTIPVENDA'] = {"$":self.codtipvenda}
-        dados_cabecalho['CODVEND'] = {"$":self.codvend}        
+        dados_cabecalho['CODNAT'] = {"$":self.dados_empresa.get('snk_codnat')}
+        dados_cabecalho['CODPARC'] = {"$":self.dados_empresa.get('snk_codparc')}
+        dados_cabecalho['CODTIPOPER'] = {"$":self.dados_empresa.get('snk_top_pedido')}
+        dados_cabecalho['CODTIPVENDA'] = {"$":self.dados_empresa.get('snk_codtipvenda')}
+        dados_cabecalho['CODVEND'] = {"$":self.dados_empresa.get('snk_codvend')}
         dados_cabecalho['DTNEG'] = {"$":data_negociacao}
         dados_cabecalho['NUNOTA'] = {},
         dados_cabecalho['TIPMOV'] = {"$":"P"}
@@ -108,7 +118,7 @@ class Pedido:
                 dados_item['PERCDESC'] = {"$":'0'}
                 dados_item['IGNOREDESCPROMOQTD'] = {"$": "True"}
                 dados_item['CODVOL'] = {"$":"UN"}
-                dados_item['CODLOCALORIG'] = {"$":self.codlocalorig}
+                dados_item['CODLOCALORIG'] = {"$":self.dados_empresa.get('snk_codlocal_venda')}
                 dados_itens.append(dados_item)
             except Exception as e:
                 logger.error("Item %s. Erro: %s",item.get('codprod'),e)
@@ -117,7 +127,12 @@ class Pedido:
 
         return dados_cabecalho, dados_itens, origem
     
-    def to_sankhya_devolucao(self, dados_olist:list, dados_sankhya:list) -> list:
+    def to_sankhya_devolucao(
+            self,
+            dados_olist:list,
+            dados_sankhya:list
+        ) -> list:
+
         dados_itens = []
 
         try:
