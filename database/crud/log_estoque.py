534e48d9c63f3aec65e8ea9fd730f3a4e0339c87
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-12 15:53:03 -0300

    crud.log_estoque: adicionado formatar_retorno

--- database/crud/log_estoque.py
+++ database/crud/log_estoque.py
@@ -2,6 +2,7 @@ from database.database import AsyncSessionLocal
 from database.models import LogEstoque
 from src.utils.log import Log
 from sqlalchemy.future import select
+from src.utils.db import formatar_retorno
 import os
 import logging
 from dotenv import load_dotenv
@@ -22,7 +23,7 @@ async def criar(
         status_estoque:bool=True,
         status_lotes:bool=None,
         obs:str=None
-    ):
+    ) -> bool:
     async with AsyncSessionLocal() as session:
         novo_log = LogEstoque(log_id=log_id,
                               codprod=codprod,
@@ -33,24 +34,25 @@ async def criar(
                               obs=obs)
         session.add(novo_log)
         await session.commit()
-        await session.refresh(novo_log)
-        return novo_log.id
+        return True
 
-async def buscar_falhas(log_id: int):
+async def buscar_falhas(log_id: int) -> list[dict]:
     async with AsyncSessionLocal() as session:
         result = await session.execute(
             select(LogEstoque)
             .where(LogEstoque.log_id == log_id,
                    LogEstoque.sucesso.is_(False))
         )
-        log = result.scalars().all()
-        return log
+        logs = result.scalars().all()
+        dados_logs = formatar_retorno(retorno=logs)        
+        return dados_logs
 
-async def buscar_id(log_id: int):
+async def buscar_id(log_id: int) -> list[dict]:
     async with AsyncSessionLocal() as session:
         result = await session.execute(
             select(LogEstoque)
             .where(LogEstoque.log_id == log_id)
         )
-        log = result.scalars().all()
-        return log
+        logs = result.scalars().all()
+        dados_logs = formatar_retorno(retorno=logs)        
+        return dados_logs
