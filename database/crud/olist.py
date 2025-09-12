220ffc5a435fb5cd63caabe3160c3ad892f1352a
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-12 16:02:07 -0300

    crud.olist: update

--- database/crud/olist.py
+++ database/crud/olist.py
@@ -24,21 +24,18 @@ async def criar(
         empresa_id:int,
         **kwargs
     ):
-
     if kwargs:
         kwargs = validar_dados(modelo=Olist,
                                kwargs=kwargs,
                                colunas_criptografadas=COLUNAS_CRIPTOGRAFADAS)
         if not kwargs:
             return False
-
     async with AsyncSessionLocal() as session:
         try:
             novo_token = Olist(empresa_id=empresa_id,
                                **kwargs)
             session.add(novo_token)
             await session.commit()
-            await session.refresh(novo_token)
             return True
         except Exception as e:
             logger.error("Erro ao salvar token no banco de dados: %s",e)
@@ -48,31 +45,24 @@ async def buscar(
         empresa_id:int=None,
         codemp:int=None
     ):
-
     if not any([empresa_id,codemp]):
         return False
-
     async with AsyncSessionLocal() as session:
-
         if empresa_id:
             result = await session.execute(
                 select(Olist).where(Olist.empresa_id == empresa_id).order_by(Olist.id.desc()).fetch(1)
             )
-
         if codemp:
             result = await session.execute(
                 select(Olist).where(Olist.empresa_.has(Empresa.snk_codemp == codemp)).order_by(Olist.id.desc()).fetch(1)
             )
-
         token = result.scalar_one_or_none()
-        if not token:
-            print("Token não encontrado")
-            return False
-        
-        dados_token = formatar_retorno(colunas_criptografadas=COLUNAS_CRIPTOGRAFADAS,
-                                       retorno=token)
-        
-        return dados_token 
+    if not token:
+        print("Token não encontrado")
+        return False        
+    dados_token = formatar_retorno(colunas_criptografadas=COLUNAS_CRIPTOGRAFADAS,
+                                    retorno=token)        
+    return dados_token 
 
 async def excluir(id:int):
     async with AsyncSessionLocal() as session:
@@ -91,15 +81,13 @@ async def excluir(id:int):
             logger.error("Erro ao excluir token do banco de dados: %s", e)
             return False
 
-async def excluir_cache():
-    
+async def excluir_cache():    
     try:
         dias = os.getenv('DIAS_LIMPA_CACHE',7)
     except Exception as e:
         erro = f"Valor para intervalo de dias do cache não encontrado. {e}"
         logger.error(erro)
-        return False
-    
+        return False    
     async with AsyncSessionLocal() as session:
         result = await session.execute(
             select(Olist).where(Olist.dh_solicitacao < (datetime.now()-timedelta(days=dias)))
