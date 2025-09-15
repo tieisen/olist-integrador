6d6bb0149af17c3c454956e916586635dea45835
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-15 10:36:37 -0300

    integrador.estoque: revisao geral

--- src/integrador/estoque.py
+++ src/integrador/estoque.py
@@ -1,10 +1,14 @@
-import logging
 import os
-from database.crud import log, log_estoque
-from src.sankhya.estoque import Estoque as EstoqueSnk
-from src.olist.estoque import Estoque as EstoqueOlist
+import logging
 from dotenv import load_dotenv
-from src.utils.log import Log
+from database.crud                 import log         as crudLog
+from database.crud                 import log_estoque as crudLogEst
+from src.sankhya.estoque           import Estoque     as EstoqueSnk
+from src.olist.estoque             import Estoque     as EstoqueOlist
+from src.utils.decorador.contexto  import contexto
+from src.utils.decorador.empresa   import ensure_dados_empresa
+from src.utils.decorador.ecommerce import ensure_dados_ecommerce
+from src.utils.log                 import Log
 
 load_dotenv('keys/.env')
 logger = logging.getLogger(__name__)
@@ -14,178 +18,187 @@ logging.basicConfig(filename=Log().buscar_path(),
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO)
 
-CONTEXTO = 'estoque'
-
 class Estoque:
 
-    def __init__(self):
-        self.log_id = None
+    def __init__(self, codemp:int=None, id_loja:int=None):
+        self.codemp = codemp
+        self.id_loja = id_loja
+        self.contexto = 'estoque'
+        self.dados_empresa = None
+        self.dados_ecommerce = None
 
-    def calcular_variacao(self, estoque_snk:list, estoque_olist:list=None) -> list:        
-        result = []
+    @contexto
+    @ensure_dados_ecommerce
+    def calcular_variacao(
+            self,
+            estoque_snk:dict,
+            estoque_olist:dict=None
+        ) -> dict:
+
+        resultado = {}
+
         if not estoque_olist:
-            try:
-                for produto_snk in estoque_snk:
-                    variacao = int(produto_snk.get('disponivel'))
-                    ajuste_estoque = {
-                            "id": int(produto_snk.get('ad_mkp_idprod')),
-                            "codprod": int(produto_snk.get('codprod')),
-                            "deposito": int(os.getenv('OLIST_ID_DEPOSITO_PADRAO')),
-                            "quantidade": variacao,
-                            "tipo": "B"
-                        }  
-                    result.append({
-                        "variacao": variacao,
-                        "ajuste_estoque": ajuste_estoque
-                    })
-            except Exception as e:
-                print(f"Erro ao calcular variação do balanço. {e}")
-                return []
-
-        if estoque_olist:
-
-            # Verifica se existe diferença entre o estoque disponível do Sankhya e do Olist
-            for produto_snk in estoque_snk:
-                for produto_olist in estoque_olist:
-                    if int(produto_snk.get('ad_mkp_idprod')) == produto_olist.get('id'):                        
-                        break
-
-                if not produto_olist.get('depositos'):
-                    logger.error("Não é possível atualizar estoque do produto %s",produto_olist.get('id'))
-                    print(f"Não é possível atualizar estoque do produto {produto_olist.get('id')}")
-                    continue                    
-                
-                if int(produto_olist.get('disponivel')) == int(produto_snk.get('disponivel')):
-                    result.append({
-                        "variacao": 0,
-                        "ajuste_estoque": {
-                            "id": int(produto_olist.get('id')),
-                            "codprod": int(produto_snk.get('codprod')),
-                            "deposito": 0,
-                            "quantidade": 0
-                        }    
-                    })                    
-                    continue
-
-                variacao = int(produto_snk.get('disponivel')) - produto_olist.get('disponivel')
-                ajuste_estoque = {
-                        "id": int(produto_olist.get('id')),
-                        "codprod": int(produto_snk.get('codprod')),
-                        "deposito": int(produto_olist.get('depositos')[0].get('id')),
-                        "quantidade":abs(variacao)
-                    }    
-                if variacao < 0:
-                    ajuste_estoque["tipo"] = "S"
-                if variacao > 0:
-                    ajuste_estoque["tipo"] = "E"
-
-                result.append({
-                    "variacao": variacao,
-                    "ajuste_estoque": ajuste_estoque
-                })
-
-        return result
-
-    def atualizar_log_lote(self, lista_estoque:list):
-        try:
-            for l in lista_estoque:            
-                log_estoque.criar(log_id=self.log_id,
-                                  codprod=l['ajuste_estoque'].get('codprod'),
-                                  idprod=l['ajuste_estoque'].get('id'),
-                                  qtdmov=l.get('variacao'),
-                                  status_estoque=l.get('sucesso'))
-            return True
-        except Exception as e:
-            logger.error("Erro ao atualizar log de estoques em lote: %s",e)
-            print(f"Erro: {e}")
+            # Produto recém cadastrado
+            variacao = int(estoque_snk.get('disponivel'))
+            ajuste_estoque = {
+                    "id": int(estoque_snk.get('ad_mkp_idprod')),
+                    "codprod": int(estoque_snk.get('codprod')),
+                    "deposito": self.dados_ecommerce.get('id_deposito'),
+                    "quantidade": variacao,
+                    "tipo": "B"
+                }
+            resultado = {
+                "variacao": variacao,
+                "ajuste_estoque": ajuste_estoque
+            }
+            return resultado
+        
+        # Se o produto é Kit ou qualquer tipo que não pode ser diretamente movimentado
+        # o Olist não informa código de depósito
+        if not estoque_olist.get('depositos'):
+            logger.error("Não é possível movimentar estoque desse tipo de produto %s",estoque_olist.get('id'))
+            print(f"Não é possível movimentar estoque desse tipo de produto {estoque_olist.get('id')}")
             return False
 
-    async def atualizar_olist(self, balanco:bool=False):
-        # Registro no log
-        self.log_id = log.criar(de='sankhya', para='olist', contexto=CONTEXTO)
+        # Verifica se existe diferença entre o estoque disponível do Sankhya e do Olist
+        if int(estoque_olist.get('disponivel')) == int(estoque_snk.get('disponivel')):
+            resultado = {
+                "variacao": 0,
+                "ajuste_estoque": {
+                    "id": int(estoque_olist.get('id')),
+                    "codprod": int(estoque_snk.get('codprod')),
+                    "deposito": 0,
+                    "quantidade": 0
+                }    
+            }
+            return resultado
 
-        estoque_snk = EstoqueSnk()
-        estoque_olist = EstoqueOlist()
-        
-        # Busca lista de produtos com alterações no Sankhya
-        print("Busca lista de produtos com alterações de estoque")
-        alteracoes_pendentes = await estoque_snk.buscar_alteracoes()
+        # Recupera o código do depósito ou busca do banco
+        try:
+            id_deposito = int(estoque_olist.get('depositos')[0].get('id'))
+        except:
+            id_deposito = self.dados_ecommerce.get('id_deposito')
 
-        print(f"{len(alteracoes_pendentes)} produtos com alteracoes de estoque")
+        # Calcula a variação do saldo do estoque
+        variacao = int(estoque_snk.get('disponivel')) - estoque_olist.get('disponivel')
+        ajuste_estoque = {
+                "id": int(estoque_olist.get('id')),
+                "codprod": int(estoque_snk.get('codprod')),
+                "deposito": id_deposito,
+                "quantidade":abs(variacao)
+            }    
         
-        if len(alteracoes_pendentes) == 0:
-            print("Sem alterações pendentes")
-            # Registro no log
-            log_estoque.criar(log_id=self.log_id,
-                              codprod=0,
-                              idprod=0,
-                              obs = "Sem alterações pendentes")
-            log.atualizar(id=self.log_id)
+        # Define se é movimento de Saída ou Entrada
+        if variacao < 0:
+            ajuste_estoque["tipo"] = "S"
+        if variacao > 0:
+            ajuste_estoque["tipo"] = "E"
+
+        resultado = {
+            "variacao": variacao,
+            "ajuste_estoque": ajuste_estoque
+        }
+
+        return resultado
+
+    async def atualizar_log_lote(self, log_id:int, lista_estoque:list):
+        try:
+            for l in lista_estoque:            
+                await crudLogEst.criar(log_id=log_id,
+                                       codprod=l['ajuste_estoque'].get('codprod'),
+                                       idprod=l['ajuste_estoque'].get('id'),
+                                       qtdmov=l.get('variacao'),
+                                       status_estoque=l.get('sucesso'))
             return True
+        except Exception as e:
+            logger.error("Erro ao atualizar log de estoques em lote: %s",e)            
+            return False
+
+    @contexto
+    @ensure_dados_empresa
+    async def atualizar_olist(self, **kwargs):
+        log_id = await crudLog.criar(empresa_id=self.dados_empresa.get('id'),
+                                     de='sankhya',
+                                     para='olist',
+                                     contexto=kwargs.get('_contexto'))        
+        # Busca lista de produtos com alterações de estoque no Sankhya
+        print("")
+        print("=========================================================")
+        print("-> Buscando de produtos com alterações de estoque no Sankhya...")
+        estoque_snk = EstoqueSnk()
+        alteracoes_pendentes = await estoque_snk.buscar_alteracoes()
+        if not alteracoes_pendentes:
+            print("Sem alterações pendentes")
+            await crudLog.atualizar(id=log_id,
+                                    sucesso=True)
+            return True
+        print(f"{len(alteracoes_pendentes)} produtos com alteracoes de estoque")
 
         # Extrai lista dos produtos
-        print("Extraindo lista dos produtos...")
+        print("-> Extraindo lista dos produtos...")
         lista_codprod = [int(produto.get('codprod')) for produto in alteracoes_pendentes]
-        lista_id = [int(produto.get('idprod')) for produto in alteracoes_pendentes]
-
-        print(f"{len(lista_codprod)} produtos para sincronizar.")
+        lista_id = [int(produto.get('idprod')) for produto in alteracoes_pendentes]     
 
         try:
-            if balanco:
-                # Busca estoque dos produtos no Sankhya
-                print("Buscando estoque dos produtos no Sankhya...")
-                dados_estoque_snk = await estoque_snk.buscar(lista_produtos=lista_codprod)
+            # Busca estoque dos produtos no Sankhya e no Olist
+            print("-> Buscando estoque dos produtos no Sankhya...")
+            dados_estoque_snk = await estoque_snk.buscar(lista_produtos=lista_codprod)
+            if not dados_estoque_snk:
+                logger.error("Erro ao buscar estoque no Sankhya")                
+                raise Exception("Erro ao buscar estoque no Sankhya")
 
-                # Calcula variação dos produtos
-                print("Calculando variação dos produtos...")
-                dados_update = self.calcular_variacao(estoque_snk=dados_estoque_snk)
-            else: 
-                # Busca estoque dos produtos no Sankhya e no Olist
-                print("Buscando estoque dos produtos no Sankhya...")
-                dados_estoque_snk = await estoque_snk.buscar(lista_produtos=lista_codprod)
-                print("Buscando estoque dos produtos no Olist...")
-                dados_estoque_olist = await estoque_olist.buscar(lista_produtos=lista_id)
+            # Busca estoque dos produtos no Olist        
+            print("-> Buscando estoque dos produtos no Olist...")
+            estoque_olist = EstoqueOlist()
+            dados_estoque_olist = await estoque_olist.buscar(lista_produtos=lista_id)
+            if not dados_estoque_olist:
+                logger.error("Erro ao buscar estoque no Olist")
+                raise Exception("Erro ao buscar estoque no Olist")
 
-                # Calcula variação dos produtos
-                print("Calculando variação dos produtos...")
-                dados_update = self.calcular_variacao(estoque_olist=dados_estoque_olist,
-                                                      estoque_snk=dados_estoque_snk)
+            # Calcula variação dos produtos
+            print("-> Calculando variação dos produtos...")
+            dados_update = self.calcular_variacao(estoque_olist=dados_estoque_olist,
+                                                estoque_snk=dados_estoque_snk)
+            if not dados_update:
+                logger.error("Erro ao calcular variação dos produtos")
+                raise Exception("Erro ao calcular variação dos produtos")
 
             # Envia modificações para Olist
-            print("Enviando modificações para Olist...")
+            print("-> Enviando modificações para Olist...")
             res_estoque = await estoque_olist.enviar_saldo(lista_dados=dados_update)
             if not res_estoque:
-                print("Erro ao enviar modificações de estoque")
                 logger.error("Erro ao enviar modificações de estoque")
-                return False
+                raise Exception("Erro ao enviar modificações de estoque")
 
             # Limpa tabela de alterações pendentes
-            print("Limpando tabela de alterações")
+            print("-> Limpando tabela de alterações...")
             ack = await estoque_snk.remover_alteracoes(lista_produtos=res_estoque)
             if not ack:
-                print("Erro ao remover alterações pendentes")
                 logger.error("Erro ao remover alterações pendentes")
+                raise Exception("Erro ao remover alterações pendentes")
 
-            print("Atualizando log")
-            if not self.atualizar_log_lote(lista_estoque=res_estoque):
-                print("Erro ao atualizar log")
-                logger.error("Erro ao atualizar log")
+            # Atualiza o log de eventos
+            print("-> Atualizando logs...")
+            if not await self.atualizar_log_lote(log_id=log_id,
+                                                 lista_estoque=res_estoque):
+                raise Exception("Erro ao atualizar log")
 
             print(f"Estoque sincronizado com sucesso!")
 
             # Registro no log
-            status_log = False if log_estoque.buscar_status_false(self.log_id) else True
-            log.atualizar(id=self.log_id, sucesso=status_log)
-
+            status_log = False if await crudLogEst.buscar_falhas(log_id) else True
+            await crudLog.atualizar(id=log_id,sucesso=status_log)
+            print("=========================")
+            print("--> INTEGRAÇÃO DE ESTOQUE CONCLUÍDA!")
             return True
         except Exception as e:
-            obs = f"Erro ao atualizar estoque no olist: {e}"
-            log_estoque.criar(log_id=self.log_id,
-                              codprod=0,
-                              idprod=0,
-                              qtdmov=0,
-                              obs=obs,
-                              status_estoque=False)            
-            log.atualizar(id=self.log_id, sucesso=False)
-            logger.error(obs)
+            obs = f"{e}"
+            await crudLogEst.criar(log_id=log_id,
+                                   codprod=0,
+                                   idprod=0,
+                                   qtdmov=0,
+                                   obs=obs,
+                                   status_estoque=False)            
+            await crudLog.atualizar(id=log_id,
+                                    sucesso=False)
             return False
