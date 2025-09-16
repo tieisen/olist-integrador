d3b25f49731c5e69dbc63cf5db27cd5837ff7b2f
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-16 08:41:21 -0300

    parser.transferencia: ajuste decorador dados empresa

--- src/parser/transferencia.py
+++ src/parser/transferencia.py
@@ -3,6 +3,8 @@ import logging
 from datetime import datetime
 from dotenv import load_dotenv
 from src.utils.log import Log
+from src.utils.decorador.empresa import ensure_dados_empresa
+from src.utils.decorador.internal_only import internal_only
 
 load_dotenv('keys/.env')
 logger = logging.getLogger(__name__)
@@ -14,37 +16,43 @@ logging.basicConfig(filename=Log().buscar_path(),
 
 class Transferencia:
 
-    def __init__(self):
-        self.codparc = os.getenv('SANKHYA_CODPARC')
+    def __init__(self, codemp:int=None, empresa_id:int=None):
+        self.codemp = codemp
+        self.empresa_id = empresa_id
+        self.dados_empresa = None
 
+    @internal_only
     def cabecalho(self):
         return {
                 "NUNOTA":  {"$": ""},
                 "SERIENOTA":  {"$": "1"},
-                "CODEMP":  {"$": "1"},
-                "CODEMPNEGOC":  {"$": "31"},
-                "CODPARC":  {"$": self.codparc},
-                "CODNAT":  {"$": "1010101"},
+                "CODEMP":  {"$": self.dados_empresa.get('snk_codemp_fornecedor')},
+                "CODEMPNEGOC":  {"$": self.dados_empresa.get('snk_codemp')},
+                "CODPARC":  {"$": self.dados_empresa.get('snk_codparc')},
+                "CODNAT":  {"$": self.dados_empresa.get('snk_codnat_transferencia')},
                 "CODTIPVENDA":  {"$": "0"},
-                "CODTIPOPER":  {"$": "1419"},
+                "CODTIPOPER":  {"$": self.dados_empresa.get('snk_top_transferencia')},
                 "CODVEND":  {"$": "1"},
                 "CODOBSPADRAO":  {"$": "0"},
                 "DTNEG": {"$":datetime.now().strftime('%d/%m/%Y')},
                 "TIPMOV":  {"$": "T"},
                 "CIF_FOB":  {"$": "C"},
                 "TIPFRETE":  {"$": "N"},
-                "OBSERVACAO":  {"$": "Transferência entre empresas para ressuprimento e atendimento de pedidos Shopee."}
+                "OBSERVACAO":  {"$": self.dados_empresa.get('snk_texto_transferencia')}
             }
     
-    def itens(self, nunota:int=None, itens_transferencia:list=None, itens_transferidos:list=None):
-        if not itens_transferencia:
-            print("Dados não informados.")
-            logger.error("Dados não informados.")
-            return False
+    @internal_only
+    def itens(
+            self,
+            itens_transferencia:list,
+            nunota:int=None,        
+            itens_transferidos:list=None
+        ) -> list[dict]:
 
         dados_item = {}
         res = []
 
+        # Se a nota de transferência está vazia, lança todos os itens recebidos no parâmetro
         if not itens_transferidos:
             for item in itens_transferencia:
                 vlrtot = round(item.get('valor') * item.get('quantidade'),3)
@@ -57,14 +65,16 @@ class Transferencia:
                 dados_item['PERCDESC'] = {"$": "0"}
                 dados_item['VLRDESC'] = {"$": "0"}
                 dados_item['CODVOL'] = {"$": 'UN'}
-                dados_item['CODLOCALORIG'] = {"$": '101'}
-                dados_item['CODLOCALDEST'] = {"$": '101'}
+                dados_item['CODLOCALORIG'] = {"$": self.dados_empresa.get('snk_codlocal_venda')}
+                dados_item['CODLOCALDEST'] = {"$": self.dados_empresa.get('snk_codlocal_venda')}
                 dados_item['CONTROLE'] = {"$": item.get('controle')}
                 res.append(dados_item)
                 dados_item = {}            
             return res
         
+        # Se a nota não está vazia...
         lista_itens_transferidos = [item.get('codprod') for item in itens_transferidos]
+        # Valida se o item já está lançado na nota e adiciona a quantidade necessária
         if item.get('codigo') in itens_transferidos:
             if not item.get('lotes'):
                 print(f"Item {item.get('codigo')} sem controle informado na nota")
@@ -84,6 +94,7 @@ class Transferencia:
                 res.append(dados_item)
                 dados_item = {}
         else:
+        # Se o item não está lançado na nota, adiciona a quantidade total
             for i in item.get('lotes'):
                 vlrtot = round(item.get('valor') * item.get('quantidade'),3)
                 dados_item['NUNOTA'] = {"$":nunota}
@@ -95,29 +106,35 @@ class Transferencia:
                 dados_item['PERCDESC'] = {"$": "0"}
                 dados_item['VLRDESC'] = {"$": "0"}
                 dados_item['CODVOL'] = {"$": item.get('unidade')}
-                dados_item['CODLOCALORIG'] = {"$": '101'}
-                dados_item['CODLOCALDEST'] = {"$": '101'}
+                dados_item['CODLOCALORIG'] = {"$": self.dados_empresa.get('snk_codlocal_venda')}
+                dados_item['CODLOCALDEST'] = {"$": self.dados_empresa.get('snk_codlocal_venda')}
                 dados_item['CONTROLE'] = {"$": i.get('lote')}
                 res.append(dados_item)
                 dados_item = {}
         return res
 
-    def to_sankhya(self, objeto:str=None, nunota:int=None, itens_transferencia:list=None, itens_transferidos:list=None):
+    @ensure_dados_empresa
+    async def to_sankhya(
+            self,
+            objeto:str,
+            nunota:int=None,
+            itens_transferencia:list=None,
+            itens_transferidos:list=None
+        ) -> tuple[dict,list[dict]]:
 
-        if not objeto:
-            print("Objeto não informado.")
-            logger.error("Objeto não informado.")
-            return False
+        dados_cabecalho = {}
+        dados_itens = []
+        retorno = (dados_cabecalho,dados_itens)
 
-        if objeto == 'cabecalho':
-            return self.cabecalho()
+        if objeto in ['cabecalho','nota']:
+            dados_cabecalho = self.cabecalho()            
         
-        if objeto == 'item':
-            return self.itens(nunota=nunota,
-                              itens_transferencia=itens_transferencia,
-                              itens_transferidos=itens_transferidos)
+        if objeto in ['item','nota']:
+            if not nunota:
+                # Se for lançar somente os itens, API exige que o cabeçalho já exista
+                return False
+            dados_itens = self.itens(nunota=nunota,
+                                     itens_transferencia=itens_transferencia,
+                                     itens_transferidos=itens_transferidos)
         
-        if objeto == 'nota':
-            return self.cabecalho(), self.itens(nunota=nunota,
-                                                itens_transferencia=itens_transferencia)
-        
+        return retorno
