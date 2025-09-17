d5667dc66aef83cef7a3b05397c955cc387e1dd0
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-17 15:45:23 -0300

    crud.nota: formatacao do retorno e remocao de funcoes

--- database/crud/nota.py
+++ database/crud/nota.py
@@ -84,7 +84,7 @@ async def buscar(
         return False
     dados_nota = formatar_retorno(colunas_criptografadas=COLUNAS_CRIPTOGRAFADAS,
                                   retorno=nota)
-    return dados_nota    
+    return dados_nota
 
 async def atualizar(
         id_nota:int=None,
@@ -105,36 +105,30 @@ async def atualizar(
             return False
             
     async with AsyncSessionLocal() as session:
-        if id_nota:
-            result = await session.execute(
-                select(Nota)
-                .where(Nota.id_nota == id_nota)
-            )
-        if chave_acesso:
+        if chave_acesso and not any([id_nota,nunota_pedido]):
             result = await session.execute(
                 select(Nota)
                 .where(Nota.chave_acesso == chave_acesso)
             )
-        if nunota_pedido:
-            result = await session.execute(
-                select(Nota)
-                .where(Nota.pedido_.has(Pedido.nunota == nunota_pedido))
-            )
+        else:
+            kwargs['chave_acesso'] = chave_acesso
+            if id_nota:
+                result = await session.execute(
+                    select(Nota)
+                    .where(Nota.id_nota == id_nota)
+                )
+            if nunota_pedido:
+                result = await session.execute(
+                    select(Nota)
+                    .where(Nota.pedido_.has(Pedido.nunota == nunota_pedido))
+                )
 
-        try:
-            nota = result.scalar_one_or_none()
-        except:
-            nota = result.scalars().all()
-        
-        if not nota:
+        notas = result.scalars().all()        
+        if not notas:
             print(f"Nota não encontrada. Parâmetro: {id_nota or chave_acesso or nunota_pedido}")
             return False
         
-        if isinstance(nota,list):
-            for n in nota:
-                for key, value in kwargs.items():
-                    setattr(n, key, value)
-        else:
+        for nota in notas:
             for key, value in kwargs.items():
                 setattr(nota, key, value)            
             
@@ -151,7 +145,7 @@ async def validar_pedido_atendido(id_pedido:int):
         pedido_atendido = result.scalar_one_or_none()
     return True if pedido_atendido else False    
 
-async def buscar_emitir(ecommerce_id:int):
+async def buscar_criar(ecommerce_id:int):
     async with AsyncSessionLocal() as session:
         result = await session.execute(
             select(Pedido)
@@ -160,9 +154,12 @@ async def buscar_emitir(ecommerce_id:int):
                    ~Pedido.nota_.any())
             .order_by(Pedido.num_pedido)
         )
-        return result.scalars().all()
+        notas = result.scalars().all()
+    dados_nota = formatar_retorno(colunas_criptografadas=COLUNAS_CRIPTOGRAFADAS,
+                                  retorno=notas)
+    return dados_nota    
     
-async def buscar_autorizar(ecommerce_id:int):
+async def buscar_emitir(ecommerce_id:int):
     async with AsyncSessionLocal() as session:
         result = await session.execute(
             select(Nota)
@@ -171,26 +168,10 @@ async def buscar_autorizar(ecommerce_id:int):
                    Nota.pedido_.has(Pedido.ecommerce_id == ecommerce_id))
             .order_by(Nota.id)
         )
-        return result.scalars().all()
-
-async def atualizar_autorizada(
-        id_nota:int,
-        chave_acesso:str,
-        dh_emissao:str=None
-    ):
-    async with AsyncSessionLocal() as session:
-        result = await session.execute(
-            select(Nota)
-            .where(Nota.id_nota == id_nota)
-        )
-        nota = result.scalar_one_or_none()
-        if not nota:
-            print(f"Nota não encontrada. Parâmetro: {id_nota}")
-            return False
-        setattr(nota, "dh_emissao", datetime.strptime(dh_emissao,'%Y-%m-%d') if dh_emissao else datetime.now())
-        setattr(nota, "chave_acesso", chave_acesso)
-        await session.commit()
-        return True
+        notas = result.scalars().all()
+    dados_nota = formatar_retorno(colunas_criptografadas=COLUNAS_CRIPTOGRAFADAS,
+                                  retorno=notas)
+    return dados_nota 
 
 async def buscar_financeiro(ecommerce_id:int):
     async with AsyncSessionLocal() as session:
@@ -200,29 +181,10 @@ async def buscar_financeiro(ecommerce_id:int):
                    Nota.id_financeiro.is_(None),
                    Nota.pedido_.has(Pedido.ecommerce_id == ecommerce_id))
         )
-        return result.scalars().all()
-
-async def atualizar_financeiro(
-        id_nota:int,
-        id_financeiro:int
-    ):
-    async with AsyncSessionLocal() as session:
-        result = await session.execute(
-            select(Nota)
-            .where(Nota.id_nota == id_nota)
-        )
-        nota = result.scalar_one_or_none()
-        if not nota:
-            print(f"Nota não encontrada. Parâmetro: {id_nota}")
-            return False
-        
-        if nota.id_financeiro:
-            print(f"Nota já possui financeiro. Parâmetro: {id_nota}")
-            return False
-        
-        setattr(nota, "id_financeiro", id_financeiro)
-        await session.commit()
-        return True
+        notas = result.scalars().all()
+    dados_nota = formatar_retorno(colunas_criptografadas=COLUNAS_CRIPTOGRAFADAS,
+                                  retorno=notas)
+    return dados_nota 
 
 async def buscar_financeiro_baixar(ecommerce_id:int):
     async with AsyncSessionLocal() as session:
@@ -233,27 +195,10 @@ async def buscar_financeiro_baixar(ecommerce_id:int):
                    Nota.dh_baixa_financeiro.is_(None),
                    Nota.pedido_.has(Pedido.ecommerce_id == ecommerce_id))
         )
-        return result.scalars().all()    
-
-async def atualizar_financeiro_baixar(
-        id_financeiro:int,
-        dh_baixa:str=None
-    ):
-    async with AsyncSessionLocal() as session:
-        result = await session.execute(
-            select(Nota)
-            .where(Nota.id_financeiro == id_financeiro)
-        )
-        nota = result.scalar_one_or_none()
-        if not nota:
-            print(f"Financiero da nota não encontrado. Parâmetro: {id_financeiro}")
-            return False
-        if nota.dh_baixa_financeiro:
-            print(f"Nota já foi baixada. Parâmetro: {id_financeiro}")
-            return False
-        setattr(nota, "dh_baixa_financeiro", datetime.strptime(dh_baixa,'%Y-%m-%d') if dh_baixa else datetime.now())
-        await session.commit()
-        return True
+        notas = result.scalars().all()
+    dados_nota = formatar_retorno(colunas_criptografadas=COLUNAS_CRIPTOGRAFADAS,
+                                  retorno=notas)
+    return dados_nota 
 
 async def buscar_atualizar_nunota(ecommerce_id:int):
     async with AsyncSessionLocal() as session:
@@ -262,45 +207,10 @@ async def buscar_atualizar_nunota(ecommerce_id:int):
             .where(Nota.nunota.is_(None),
                    Nota.pedido_.has(Pedido.ecommerce_id == ecommerce_id))
         )
-        return result.scalars().all()
-
-async def atualizar_nunota(
-        nunota:int,
-        id_nota:int=None,
-        nunota_pedido:int=None
-    ):
-    async with AsyncSessionLocal() as session:
-
-        if nunota == nunota_pedido:
-            print("Nº único do pedido e da nota não podem ser iguais")
-            return False
-        
-        if id_nota:
-            result = await session.execute(
-                select(Nota)
-                .where(Nota.id_nota == id_nota)
-            )            
-            nota = result.scalar_one_or_none()
-            if not nota:
-                print(f"Nota não encontrada. Parâmetro: {id_nota}")
-                return False
-            setattr(nota, "nunota", nunota)
-            await session.commit()
-            return True
-
-        if nunota_pedido:
-            result = await session.execute(
-                select(Nota)
-                .where(Nota.pedido_.has(Pedido.nunota == nunota_pedido))
-            )
-            notas = result.scalars().all()
-            if not notas:
-                print(f"Notas não encontradas. Parâmetro: {nunota_pedido}")
-                return False
-            for nota in notas:
-                setattr(nota, "nunota", nunota)
-            await session.commit()
-            return True
+        notas = result.scalars().all()
+    dados_nota = formatar_retorno(colunas_criptografadas=COLUNAS_CRIPTOGRAFADAS,
+                                  retorno=notas)
+    return dados_nota 
 
 async def buscar_confirmar(ecommerce_id:int):
     async with AsyncSessionLocal() as session:
@@ -312,42 +222,9 @@ async def buscar_confirmar(ecommerce_id:int):
                    Nota.pedido_.has(Pedido.ecommerce_id == ecommerce_id))
         )
         notas = result.scalars().all()
-        return notas
-    
-async def atualizar_confirmada(
-        nunota:int,
-        dh_confirmacao:str=None
-    ):
-    async with AsyncSessionLocal() as session:
-        result = await session.execute(
-            select(Nota)
-            .where(Nota.nunota == nunota)
-        )
-        notas = result.scalars().all()
-        if not notas:
-            print(f"Nota não encontrada. Parâmetro: {nunota}")
-            return False
-        for nota in notas:
-            setattr(nota, "dh_confirmacao", datetime.strptime(dh_confirmacao,'%d/%m/%Y') if dh_confirmacao else datetime.now())
-        await session.commit()
-        return True
-
-async def atualizar_cancelamento(
-        id_nota:int,
-        dh_cancelamento:str=None
-    ):
-    async with AsyncSessionLocal() as session:
-        result = await session.execute(
-            select(Nota)
-            .where(Nota.id_nota == id_nota)
-        )
-        nota = result.scalar_one_or_none()
-        if not nota:
-            print(f"Nota não encontrada. Parâmetro: {id_nota}")
-            return False
-        setattr(nota, "dh_cancelamento", datetime.strptime(dh_cancelamento,'%d/%m/%Y') if dh_cancelamento else datetime.now())
-        await session.commit()
-        return True
+    dados_nota = formatar_retorno(colunas_criptografadas=COLUNAS_CRIPTOGRAFADAS,
+                                  retorno=notas)
+    return dados_nota 
 
 async def buscar_cancelar(ecommerce_id:int):
     async with AsyncSessionLocal() as session:
@@ -358,18 +235,6 @@ async def buscar_cancelar(ecommerce_id:int):
                    Nota.pedido_.has(Pedido.ecommerce_id == ecommerce_id))
         )
         notas = result.scalars().all()
-        return notas
-
-async def atualizar_cancelamento_sankhya(id_nota:int):
-    async with AsyncSessionLocal() as session:
-        result = await session.execute(
-            select(Nota)
-            .where(Nota.id_nota == id_nota)
-        )
-        nota = result.scalar_one_or_none()
-        if not nota:
-            print(f"Nota não encontrada. Parâmetro: {id_nota}")
-            return False
-        setattr(nota, "cancelado_sankhya", True)
-        await session.commit()
-        return True    
+    dados_nota = formatar_retorno(colunas_criptografadas=COLUNAS_CRIPTOGRAFADAS,
+                                  retorno=notas)
+    return dados_nota 
