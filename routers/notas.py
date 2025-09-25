ddac198f6e27b5805f54d315838e235376e3ef06
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-25 14:31:36 -0300

    router.notas: cancelamento

--- routers/notas.py
+++ routers/notas.py
@@ -9,11 +9,20 @@ nota = Nota()
 def default():
     return {"message": "Notas"}
 
-@router.get("/baixar-contas")
-def baixar_contas():
+@router.get("/{id}/baixar-contas")
+def baixar_contas(id:int):
     """
     Baixa contas a receber que estão pendentes.
     """
-    if not asyncio.run(nota.baixar_financeiro()):
+    if not asyncio.run(nota.baixar_conta(id_nota=id)):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao baixar contas a receber")
+    return True
+
+@router.get("/cancelar/{numero}")
+def cancelar_nota(numero:int):
+    """
+    Cancela nota de devolução
+    """
+    if not asyncio.run(nota.integrar_cancelamento(numero=numero)):
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao cancelar nota")
     return True
