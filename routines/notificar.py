126bcbc30dafccd4391fa4f91a0ad12ed4bab438
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-25 11:18:12 -0300

    routines.notificar: revisao geral

--- routines/notificar.py
+++ routines/notificar.py
@@ -1,8 +1,18 @@
 import asyncio
 from src.services.smtp import Email
+from database.crud import empresa
 
 # ROTINA A SER EXECUTADA DIARIAMENTE, A CADA 1H
 
-if __name__=="__main__":    
+if __name__=="__main__":
+    empresas:list[dict]=[]
+    emp:dict={}
+    
+    empresas = asyncio.run(empresa.buscar())
     email = Email()
-    asyncio.run(email.notificar())
+
+    print("===================: ALERTAS DO INTEGRADOR :===================")    
+
+    for i, emp in enumerate(empresas):
+        print(f"\nEmpresa {emp.get('nome')} ({i+1}/{len(emp)})".upper())
+        asyncio.run(email.notificar(empresa_id=emp.get('id')))
