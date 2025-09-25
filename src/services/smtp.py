6320c580147e95e4baedf0de2342418403331feb
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-25 11:17:33 -0300

    services.smtp: revisao geral

--- src/services/smtp.py
+++ src/services/smtp.py
@@ -4,7 +4,7 @@ import smtplib
 from dotenv import load_dotenv
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from database.crud import log, log_estoque, log_pedido, log_produto
+from database.crud import log
 from src.utils.log import Log
 
 load_dotenv('keys/.env')
@@ -25,53 +25,23 @@ class Email:
         self.pwd = os.getenv('SENDER_PASSWORD')
         self.default_to = os.getenv('TO_DEFAULT')
 
-    def buscar_historico(self):
-        historico = log.buscar_falhas()
+    def formatar_corpo(self,historico:list[dict]):
 
+        body:str=''
+        
         if not historico:
             return False
-
-        body = ''
-
         body+='<ul style="list-style: none;">'
         for h in historico:
-            body+=f'<li>{h.dh_execucao.strftime('%H:%M')} | {h.contexto.capitalize()} | {h.de.capitalize()} > {h.para.capitalize()}</li>'
-            match h.contexto:
-                case 'estoque':            
-                    estoque = log_estoque.buscar_id(log_id=h.id)
-                    if not estoque:
-                        body+='<ul><li>Detalhamento não encontrado</li></ul>'
-                    else:
-                        body+='<ul>'
-                        for e in estoque:
-                            if bool(e.status_estoque):
-                                continue
-                            body+=f'<li>{e.obs}</li>'
-                        body+='</ul>'
-                case 'pedido':
-                    pedido = log_pedido.buscar_id(log_id=h.id)
-                    if not pedido:
-                        body+='<ul><li>Detalhamento não encontrado</li></ul>'
-                    else:
-                        body+='<ul>'
-                        for pd in pedido:
-                            if bool(pd.status):
-                                continue
-                            body+=f'<li>{pd.obs}</li>'
-                        body+='</ul>'
-                case 'produto':
-                    produto = log_produto.buscar_id(log_id=h.id)
-                    if not produto:
-                        body+='<ul><li>Detalhamento não encontrado</li></ul>'
-                    else:
-                        body+='<ul>'
-                        for p in produto:
-                            if bool(p.sucesso):
-                                continue
-                            body+=f'<li>{p.obs}</li>'
-                        body+='</ul>'
+            body+=f'<li>{h.get('hora').strftime('%H:%M')} | {h.get('contexto').capitalize()} | {h.get('de').capitalize()} > {h.get('para').capitalize()}</li>'
+            if not h.get('falhas'):
+                body+='<ul><li>Detalhamento não encontrado</li></ul>'
+            else:
+                body+='<ul>'
+                for f in h.get('falhas'):                    
+                    body+=f'<li>{f.get('obs')}</li>' if not f.get('sucesso') else ''
+                body+='</ul>'
         body+='</ul>'
-
         return body
 
     async def enviar(self,destinatario:str=None, corpo=None, assunto:str=None):
@@ -96,15 +66,22 @@ class Email:
             servidor.login(self.email, self.pwd)
             servidor.send_message(msg)
             servidor.quit()
+            print(f"E-mail enviado com sucesso para {destinatario}!")
             return True
         except Exception as e:
-            logger.error("Falha ao enviar e-mail: %s",e)
+            msg = f"Falha ao enviar e-mail: {e}"
+            logger.error(msg)
+            print(msg)
             return False
 
-    async def notificar(self, destinatario:str=None, tipo:str='erro'):
+    async def notificar(self, empresa_id:int, destinatario:str=None):
 
-        body = self.buscar_historico()
-        if not body:
+        historico:list[dict]=[]
+        corpo:str=''
+
+        historico = await log.listar_falhas(empresa_id=empresa_id)
+        corpo = self.formatar_corpo(historico=historico)
+        if not corpo:
             return False
         
         assunto = None
@@ -114,11 +91,13 @@ class Email:
             with open(file=self.email_body_path, mode='r', encoding='utf-8') as f:
                 estrutura = f.read()
 
-        assunto = os.getenv('MAIL_SUBJECT')
+        if not estrutura:
+            return False
+        
+        assunto = '[TESTE] '+os.getenv('MAIL_SUBJECT')
         cor = os.getenv('MAIL_COLOR')
         
-        await self.enviar(destinatario=destinatario or self.default_to,
-                          corpo=estrutura.format(cor,body),
-                          assunto=assunto)
-        
-        return True
+        ack = await self.enviar(destinatario=destinatario or self.default_to,
+                                corpo=estrutura.format(cor,corpo),
+                                assunto=assunto)
+        return ack
