58aaefa3d123e98cbd4de5043b41601fb4526922
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-29 17:28:30 -0300

    utils.log: funcao para configurar o logger

--- src/utils/log.py
+++ src/utils/log.py
@@ -1,14 +1,22 @@
 import os
+import logging
 from datetime import datetime
+from src.utils.load_env import load_env
 
-class Log:
+load_env()
 
-    def __init__(self):
-        pass
+def buscar_path() -> str:
+    mes_atual = datetime.now().strftime('%Y%m')
+    if not os.path.exists(f"./logs/{mes_atual}.log"):
+        with open(f"./logs/{mes_atual}.log", "w") as f:
+            pass
+    return f"./logs/{mes_atual}.log"
 
-    def buscar_path(self):
-        mes_atual = datetime.now().strftime('%Y%m')
-        if not os.path.exists(f"./logs/{mes_atual}.log"):
-            with open(f"./logs/{mes_atual}.log", "w") as f:
-                pass
-        return f"./logs/{mes_atual}.log"
+def set_logger(name:str) -> logging:   
+    logger = logging.getLogger(name)
+    logging.basicConfig(filename=buscar_path(),
+                        encoding='utf-8',
+                        format=os.getenv('LOGGER_FORMAT'),
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO)
+    return logger
