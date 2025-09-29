ac7a47a2a5e871a923bb5ed491016b320e6b760a
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-29 13:40:06 -0300

    routines.produtos: codigo convertido pra funcao

--- routines/produtos.py
+++ routines/produtos.py
@@ -5,17 +5,21 @@ from src.integrador.produto import Produto
 # ROTINA A SER EXECUTADA DIARIAMENTE, A CADA 15 MINUTOS
 # ANTES DA ROTINA DE ESTOQUE
 
-if __name__=="__main__":
+async def integrar_produtos(codemp:int=None):
 
     empresas:list[dict]=[]
 
-    empresas = asyncio.run(empresa.buscar())
+    empresas = await empresa.buscar(codemp=codemp)
 
     print("===================: INTEGRAÇÃO DE PRODUTOS :===================")
 
     for i, emp in enumerate(empresas):
         print(f"\nEmpresa {emp.get('nome')} ({i+1}/{len(emp)})".upper())
         produto = Produto(codemp=emp.get('snk_codemp'))        
-        asyncio.run(produto.receber_alteracoes())
-        asyncio.run(produto.integrar_olist())
-        asyncio.run(produto.integrar_snk())
+        await produto.receber_alteracoes()
+        await produto.integrar_olist()
+        await produto.integrar_snk()
+
+if __name__=="__main__":
+
+    asyncio.run(integrar_produtos())
