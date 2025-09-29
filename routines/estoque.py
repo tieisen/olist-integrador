b62e042c2edfd527b533168fa516f87553899003
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-29 13:40:27 -0300

    routines.estoque: codigo convertido pra funcao

--- routines/estoque.py
+++ routines/estoque.py
@@ -5,15 +5,18 @@ from src.integrador.estoque import Estoque
 # ROTINA A SER EXECUTADA DIARIAMENTE, A CADA 15 MINUTOS
 # APÓS A ROTINA DE PRODUTOS E ANTES DA ROTINA DE PEDIDOS
 
-if __name__=="__main__":
-
+async def integrar_estoque(codemp:int=None):
+    
     empresas:list[dict]=[]
-
-    empresas = asyncio.run(empresa.buscar())
+    empresas = await empresa.buscar(codemp=codemp)
 
     print("===================: INTEGRAÇÃO DE ESTOQUE :===================")    
 
     for i, emp in enumerate(empresas):
         print(f"\nEmpresa {emp.get('nome')} ({i+1}/{len(emp)})".upper())
         estoque = Estoque(codemp=emp.get('snk_codemp'))        
-        asyncio.run(estoque.atualizar_olist())
+        await estoque.atualizar_olist()
+
+if __name__=="__main__":
+
+    asyncio.run(integrar_estoque())
