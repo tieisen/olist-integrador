9384d5587c8dfbdc42dafa67584431258701e996
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-09-29 13:44:38 -0300

    routers.produtos: endpoint com codigo da empresa

--- routers/produtos.py
+++ routers/produtos.py
@@ -3,19 +3,19 @@ from src.integrador.produto import Produto
 import asyncio
 
 router = APIRouter()
-produto = Produto()
 
 @router.get("/")
 def default():
     return {"message": "Produtos"}
 
-@router.get("/integrar")
-def integrar_produtos():
+@router.get("/{codemp}/integrar")
+def integrar_produtos(codemp:int):
     """
     Cria o cadastro de produtos novos no Olist.
     Atualiza no Olist os dados dos produtos que tiveram alteração no cadastro do Sankhya.
     Atualiza no Sankhya os dados dos produtos que tiveram alteração no cadastro do Olist.
     """
+    produto = Produto(codemp=codemp)    
     if not asyncio.run(produto.receber_alteracoes()):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao receber alterações nos cadastros de produtos do Olist")
     if not asyncio.run(produto.integrar_olist()):
