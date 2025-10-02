efdf211dfa099ef446f1a25e2900a83bf65ec348
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-10-02 14:37:46 -0300

    parser.estoque: update

--- src/parser/estoque.py
+++ src/parser/estoque.py
@@ -10,9 +10,9 @@ class Estoque:
     def __init__(self):
         pass
 
-    def to_olist(self, data:dict) -> tuple[int,dict]:
+    def to_olist(self, dados_estoque:dict) -> tuple[int,dict]:
         
-        if not isinstance(data, dict):
+        if not isinstance(dados_estoque, dict):
             logger.error("Dados inválidos, deve ser um dicionário.")
             print("Dados inválidos, deve ser um dicionário.")
             return False, None
@@ -27,13 +27,13 @@ class Estoque:
 
         try:
             new_data = modelo_api.get('post')
-            new_data['deposito']['id'] = data.get('deposito')
-            new_data['tipo'] = data.get('tipo')
+            new_data['deposito']['id'] = dados_estoque.get('deposito')
+            new_data['tipo'] = dados_estoque.get('tipo')
             new_data['data'] = None
-            new_data['quantidade'] = data.get('quantidade')
+            new_data['quantidade'] = dados_estoque.get('quantidade')
             new_data['precoUnitario'] = 0
             new_data['observacoes'] = os.getenv('OLIST_OBS_MVTO_ESTOQUE','Ajuste de estoque Sankhya')
-            return data.get('id'), new_data
+            return dados_estoque.get('id'), new_data
         except Exception as e:
             logger.error("Erro ao atribuir valores ao dicionário. %s",e)
             print("Erro ao atribuir valores ao dicionário.",e)
