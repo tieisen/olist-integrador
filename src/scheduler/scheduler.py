03c9672c725b1448fecc94472da7bb9d67ec9a8c
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-10-08 08:15:56 -0300

    scheduler: correcao chamada async

--- src/scheduler/scheduler.py
+++ src/scheduler/scheduler.py
@@ -11,7 +11,12 @@ logger = set_logger(__name__)
 
 
 # Engine síncrono apenas para APScheduler
-job_engine = create_engine(os.getenv('ALEMBIC_URL'))
+db_url = os.getenv('ALEMBIC_URL')
+if not db_url:
+    raise FileNotFoundError("URL do banco de dados não encontada")
+else:
+    print(f"Conectando ao banco {db_url}")
+job_engine = create_engine(db_url)
 
 jobstores = {
     "default": SQLAlchemyJobStore(engine=job_engine)
@@ -19,7 +24,7 @@ jobstores = {
 
 scheduler = AsyncIOScheduler(jobstores=jobstores, timezone=timezone("America/Sao_Paulo"))
 
-def inicializar_tarefas():
+async def inicializar_tarefas():
     jobs_existentes = [job.id for job in scheduler.get_jobs()]
 
     async def rotina_produtos_estoque():
@@ -33,25 +38,27 @@ def inicializar_tarefas():
     #     scheduler.add_job(estoque.integrar_estoque(), "interval", minutes=10, id="sincronizar_estoque")
     
     if "sincronizar_produtos_estoque" not in jobs_existentes:
-        scheduler.add_job(estoque.rotina_produtos_estoque(), "interval", minutes=10, id="sincronizar_produtos_estoque")
+        scheduler.add_job(await rotina_produtos_estoque(), "interval", minutes=10, id="sincronizar_produtos_estoque")
 
     if "sincronizar_pedidos" not in jobs_existentes:
-        scheduler.add_job(pedidos.receber_pedido_lote(), "interval", minutes=15, id="sincronizar_pedidos")
+        scheduler.add_job(await pedidos.receber_pedido_lote(), "interval", minutes=15, id="sincronizar_pedidos")
 
     if "sincronizar_devolucoes" not in jobs_existentes:
-        scheduler.add_job(devolucoes.integrar_devolucoes(), "cron", hour=19, id="sincronizar_devolucoes")
+        scheduler.add_job(await devolucoes.integrar_devolucoes(), "cron", hour=19, id="sincronizar_devolucoes")
 
     if "notificar_erros" not in jobs_existentes:
-        scheduler.add_job(notificar.enviar_notificacao(), "interval", hour=4, id="notificar_erros")
+        scheduler.add_job(await notificar.enviar_notificacao(), "interval", hour=4, id="notificar_erros")
 
     if "limpar_cache" not in jobs_existentes:
-        scheduler.add_job(limpar_cache.excluir_cache(), "cron", day='1,15', hour=23, id="limpar_cache")
+        scheduler.add_job(await limpar_cache.excluir_cache(), "cron", day='1,15', hour=23, id="limpar_cache")
 
-def iniciar_agendador():
-    inicializar_tarefas()
+async def iniciar_agendador():
+    await inicializar_tarefas()
     scheduler.start()
     logger.info("✅ APScheduler iniciado")
+    print("✅ APScheduler iniciado")
 
-def encerrar_agendador():
+async def encerrar_agendador():
     scheduler.shutdown(wait=False)
     logger.info("🛑 APScheduler encerrado")
+    print("🛑 APScheduler encerrado")
