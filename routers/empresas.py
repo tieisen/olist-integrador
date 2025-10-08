0273a026ef84507e3230747c23b42aa8aef23331
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-10-08 14:04:09 -0300

    routers.empresas: implementada rota criar

--- routers/empresas.py
+++ routers/empresas.py
@@ -1,13 +1,11 @@
 from fastapi import APIRouter, HTTPException, status
 from src.sankhya.empresa import Empresa
+from database.schemas import EmpresaCreate, EmpresaDB
+from database.crud import empresa as crud
 import asyncio
 
 router = APIRouter()
 empresa = Empresa()
-
-@router.get("")
-def default():
-    return {"message": "Empresas"}
 
 @router.get("/buscar")
 def buscar_todas():
@@ -26,3 +24,22 @@ def buscar_codemp(codemp:int):
     if not asyncio.run(empresa.buscar(codemp=codemp)):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Erro ao buscar dados da empresa {codemp}")
     return True
+
+@router.post("")
+async def criar(empresa: EmpresaCreate) -> EmpresaDB:
+    """
+    Cadastra empresa
+    """
+    try:
+        sucesso = await crud.criar(**empresa.model_dump())
+        if not sucesso:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Erro ao criar empresa"
+            )
+        return True
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
