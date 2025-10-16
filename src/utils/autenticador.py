b83a4687c4d8ebf6e0e16346508c649ba9035692
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-10-16 10:23:13 -0300

    utils.autenticador: sempre busca o token atualizado

--- src/utils/autenticador.py
+++ src/utils/autenticador.py
@@ -4,31 +4,27 @@ from src.sankhya.autenticacao import Autenticacao as AuthSnk
 
 # BUSCA TOKEN OLIST
 async def buscar_token_olist(self):
-    if not self.token:
-        token = await AuthOlist(codemp=self.codemp,empresa_id=self.empresa_id).autenticar()
-        self.token = token.strip('"')
+    token = await AuthOlist(codemp=self.codemp,empresa_id=self.empresa_id).autenticar()
+    self.token = token.strip('"')
 
 def token_olist(func):
     @wraps(func)
     async def wrapper(self, *args, **kwargs):
         # Garante que os dados da empresa estão carregados
-        if not self.token:
-            await buscar_token_olist(self)
+        await buscar_token_olist(self)
         return await func(self, *args, **kwargs)
     return wrapper    
 
 # BUSCA TOKEN SANKHYA
 async def buscar_token_snk(self):
-    if not self.token:
-        token = await AuthSnk(codemp=self.codemp,empresa_id=self.empresa_id).autenticar()
-        self.token = token.strip('"')
+    token = await AuthSnk(codemp=self.codemp,empresa_id=self.empresa_id).autenticar()
+    self.token = token.strip('"')
 
 def token_snk(func):
     @wraps(func)
     async def wrapper(self, *args, **kwargs):
         # Garante que os dados da empresa estão carregados
-        if not self.token:
-            await buscar_token_snk(self)
+        await buscar_token_snk(self)
         return await func(self, *args, **kwargs)
     return wrapper
 
