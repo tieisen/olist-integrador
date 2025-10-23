0cee3da7412bca1bb697b548dad89206bc414047
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-10-23 10:41:35 -0300

    routers.produto: revisao async

--- routers/produtos.py
+++ routers/produtos.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, HTTPException, status
 from src.scheduler.jobs.produtos import integrar_produtos
-import asyncio
 from pydantic import BaseModel
 
 router = APIRouter()
@@ -9,12 +8,14 @@ class ProdutoModel(BaseModel):
     codemp:int
 
 @router.post("/integrar")
-def integrar_produtos(produto:ProdutoModel):
+async def integrar(produto:ProdutoModel):
     """
     Cria o cadastro de produtos novos no Olist.
     Atualiza no Olist os dados dos produtos que tiveram alteração no cadastro do Sankhya.
     Atualiza no Sankhya os dados dos produtos que tiveram alteração no cadastro do Olist.
     """
-    if not asyncio.run(integrar_produtos(codemp=produto.codemp)):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro integrar produtos")
-    return True
+    res:bool=None
+    res = await integrar_produtos(codemp=produto.codemp)
+    if not res:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao integrar produtos")
+    return res
