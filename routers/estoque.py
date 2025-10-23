af5c962c67c233f778294668388c7e18a58b8733
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-10-23 10:41:22 -0300

    routers.estoque: revisao async

--- routers/estoque.py
+++ routers/estoque.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, HTTPException, status
 from src.scheduler.jobs.estoque import integrar_estoque
-import asyncio
 from pydantic import BaseModel
 
 router = APIRouter()
@@ -9,10 +8,12 @@ class EstoqueModel(BaseModel):
     codemp:int
 
 @router.post("/integrar")
-def integrar(estoque:EstoqueModel):
+async def integrar(estoque:EstoqueModel):
     """
     Atualiza o saldo de estoque no Olist dos produtos que tiveram alteração no saldo de estoque do Sankhya.
     """
-    if not asyncio.run(integrar_estoque(codemp=estoque.codemp)):
+    res:bool=None
+    res = await integrar_estoque(codemp=estoque.codemp)
+    if not res:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao integrar estoque")
-    return True
+    return res
