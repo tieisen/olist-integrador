b796f39e857d5bb1e8b2c3883be760f62e60f221
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-10-23 10:21:25 -0300

    routers.pedidos: revisao async

--- routers/pedidos.py
+++ routers/pedidos.py
@@ -2,51 +2,59 @@ from fastapi import APIRouter, HTTPException, status
 from src.integrador.pedido import Pedido
 from src.scheduler.jobs.pedidos import integrar_pedidos, receber_pedido_lote, receber_pedido_unico, integrar_separacoes
 from src.scheduler.jobs.faturamento import integrar_faturamento, integrar_faturamento_olist, integrar_faturamento_snk, integrar_venda_interna
-import asyncio
 from pydantic import BaseModel
 
 router = APIRouter()
 
-class PedidoModel(BaseModel):
+class PedidoEmpresa(BaseModel):
     codemp:int
+
+class PedidoLoja(BaseModel):
+    id_loja:int
+
+class PedidoReceber(BaseModel):
     id_loja:int
     numero:int
+
+class PedidoAnular(BaseModel):
+    codemp:int
     nunota:int
 
 @router.post("/integrar-lote")
-def integrar(pedido:PedidoModel):
+async def integrar(body:PedidoEmpresa):
     """
     Busca os pedidos novos do Olist que estão com status Preparando Envio.
     Importa os pedidos pendentes do Olist.
     Confirma o pedido no Sankhya.
     """
     res:dict={}
-    res = asyncio.run(receber_pedido_lote(codemp=pedido.codemp))
+    res = await receber_pedido_lote(codemp=body.codemp)
     if not res.get('status'):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao integrar pedidos")
-    res = asyncio.run(integrar_pedidos(codemp=pedido.codemp))
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=res.get('exception'))
+    
+    res = await integrar_pedidos(codemp=body.codemp)
     if not res.get('status'):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao integrar pedidos")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=res.get('exception'))
     return True
 
 @router.post("/integrar-loja")
-def integrar_loja(pedido:PedidoModel):
+async def integrar_loja(body:PedidoLoja):
     """
     Busca os pedidos novos do Olist, por E-commerce, que estão com status Preparando Envio.
     Importa os pedidos pendentes do Olist.
     Confirma o pedido no Sankhya.
     """
     res:dict={}
-    res = asyncio.run(receber_pedido_lote(id_loja=pedido.id_loja))
+    res = await receber_pedido_lote(id_loja=body.id_loja)
     if not res.get('status'):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao integrar pedidos")
-    res = asyncio.run(integrar_pedidos(id_loja=pedido.id_loja))
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=res.get('exception'))
+    res = await integrar_pedidos(id_loja=body.id_loja)
     if not res.get('status'):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao integrar pedidos")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=res.get('exception'))
     return True
 
 @router.post("/faturar-lote")
-def faturar(pedido:PedidoModel):
+async def faturar(body:PedidoEmpresa):
     """
     Emite e autoriza as NFs dos pedidos no Olist.
     Envia os pedidos para separação no Olist.
@@ -55,13 +63,13 @@ def faturar(pedido:PedidoModel):
     Confirma a nota gerada no Sankhya.
     """
     res:dict={}
-    res = asyncio.run(integrar_faturamento(codemp=pedido.codemp))
+    res = await integrar_faturamento(codemp=body.codemp)
     if not res.get('status'):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao faturar pedidos")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=res.get('exception'))
     return True
 
 @router.post("/faturar-loja")
-def faturar_loja(pedido:PedidoModel):
+async def faturar_loja(body:PedidoLoja):
     """
     Emite e autoriza as NFs dos pedidos no Olist, por E-commerce.
     Envia os pedidos para separação no Olist.
@@ -70,87 +78,89 @@ def faturar_loja(pedido:PedidoModel):
     Confirma a nota gerada no Sankhya.
     """
     res:dict={}
-    res = asyncio.run(integrar_faturamento(id_loja=pedido.id_loja))
+    res = await integrar_faturamento(id_loja=body.id_loja)
     if not res.get('status'):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao faturar pedidos")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=res.get('exception'))
     return True
 
 @router.post("/faturar/olist")
-def faturar_olist(pedido:PedidoModel):
+async def faturar_olist(body:PedidoEmpresa):
     """
     Emite e autoriza as NFs dos pedidos no Olist.
     Envia os pedidos para separação no Olist.
     """
     res:dict={}
-    res = asyncio.run(integrar_faturamento_olist(codemp=pedido.codemp))    
+    res = await integrar_faturamento_olist(codemp=body.codemp)
     if not res.get('status'):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao faturar pedidos no Olist")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=res.get('exception'))
     return True
 
 @router.post("/faturar-loja/olist")
-def faturar_olist_loja(pedido:PedidoModel):
+async def faturar_olist_loja(body:PedidoLoja):
     """
     Emite e autoriza as NFs dos pedidos no Olist, por E-commerce.
     Envia os pedidos para separação no Olist.
     """
     res:dict={}
-    res = asyncio.run(integrar_faturamento_olist(id_loja=pedido.id_loja))    
+    res = await integrar_faturamento_olist(id_loja=body.id_loja)
     if not res.get('status'):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao faturar pedidos no Olist")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=res.get('exception'))
     return True
 
 @router.post("/faturar/snk")
-def faturar_sankhya(pedido:PedidoModel):
+async def faturar_sankhya(body:PedidoEmpresa):
     """
     Cria e confirma a nota de transferência no Sankhya.
     Fatura o pedido no Sankhya.
     Confirma a nota gerada no Sankhya.
     """    
     res:dict={}
-    res = asyncio.run(integrar_faturamento_snk(codemp=pedido.codemp))    
+    res = await integrar_faturamento_snk(codemp=body.codemp)
     if not res.get('status'):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao faturar pedidos no Sankhya")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=res.get('exception'))
     return True
 
 @router.post("/faturar/venda-interna")
-def faturar_venda_interna(pedido:PedidoModel):
+async def faturar_venda_interna(body:PedidoEmpresa):
     """
     Cria e confirma a nota de transferência no Sankhya.
     """
     res:dict={}
-    res = asyncio.run(integrar_venda_interna(codemp=pedido.codemp))    
+    res = await integrar_venda_interna(codemp=body.codemp)
     if not res.get('status'):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao criar venda interna")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=res.get('exception'))
     return True
 
 @router.post("/receber")
-def receber_pedido(pedido:PedidoModel):
+async def receber_pedido(body:PedidoReceber):
     """
     Busca um pedido específico do Olist
     """
     res:dict={}
-    res = asyncio.run(receber_pedido_unico(id_loja=pedido.id_loja,numero=pedido.numero)) 
+    res = await receber_pedido_unico(id_loja=body.id_loja,numero=body.numero)
     if not res.get('status'):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Erro ao buscar pedido {pedido.numero}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=res.get('exception'))
     return True
 
 @router.post("/separacao/buscar")
-def buscar_separacao(pedido:PedidoModel):
+async def buscar_separacao(body:PedidoEmpresa):
     """
     Busca as separações pendentes no Olist
     """
     res:dict={}
-    res = asyncio.run(integrar_separacoes(codemp=pedido.codemp)) 
+    res = await integrar_separacoes(codemp=body.codemp)
     if not res.get('status'):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao buscar separação dos pedidos")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=res.get('exception'))
     return True
 
 @router.post("/anular")
-def anular_pedidos(pedido:PedidoModel):
+async def anular_pedidos(body:PedidoAnular):
     """
     Exclui pedido não faturado do Sankhya
     """
-    _pedido = Pedido(codemp=pedido.codemp)
-    if not asyncio.run(_pedido.anular_pedido_importado(nunota=pedido.nunota)):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao anular pedido")
+    res:dict={}    
+    _pedido = Pedido(codemp=body.codemp)
+    res = await _pedido.anular_pedido_importado(nunota=body.nunota)
+    if not res.get('sucesso'):
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=res.get('__exception__'))
     return True
