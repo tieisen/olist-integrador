dc5f14ea6ad00fe6f28cfc2e6876fb2aec24f982
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-11-10 14:02:25 -0300

    parser.pedido: implementado pedido_venda, atualiza_local, baixa_estoque_ecommerce

--- src/parser/pedido.py
+++ src/parser/pedido.py
@@ -80,7 +80,6 @@ class Pedido:
         ) -> tuple[dict,list]:
 
         def formatar_pedidos(lista_pedidos):
-            # linhas = [f"- {pedido['numero']}/{pedido['codigo']}" for pedido in lista_pedidos]
             linhas = [f"{pedido['numero']}" for pedido in lista_pedidos]
             return f"Referente à {len(lista_pedidos)} pedidos:\n" + ", ".join(linhas)        
 
@@ -120,8 +119,132 @@ class Pedido:
                 logger.error("Item %s. Erro: %s",item.get('codprod'),e)
                 print(f"Item {item.get('codprod')}. Erro: {e}")
                 continue
+            
+        return dados_cabecalho, dados_itens
+    
+    @carrega_dados_ecommerce
+    @carrega_dados_empresa
+    async def to_sankhya_pedido_venda(
+            self,
+            lista_itens:list
+        ) -> tuple[dict,list]:
+
+        dados_cabecalho = {}
+        dados_itens = []
+
+        data_negociacao = datetime.now().strftime('%d/%m/%Y')
+        observacao = f"Itens de ressuprimento para vendas da loja {self.dados_ecommerce.get('nome')}"
+        
+        dados_cabecalho['AD_MKP_ORIGEM'] = {"$":self.dados_ecommerce.get('id_loja')}
+        dados_cabecalho['CIF_FOB'] = {"$":'C'}
+        dados_cabecalho['CODCENCUS'] = {"$":'0'}
+        dados_cabecalho['CODEMP'] = {"$":self.dados_empresa.get('snk_codemp_fornecedor')}
+        dados_cabecalho['CODNAT'] = {"$":self.dados_empresa.get('snk_codnat')}
+        dados_cabecalho['CODPARC'] = {"$":self.dados_empresa.get('snk_codparc')}
+        dados_cabecalho['CODTIPOPER'] = {"$":self.dados_empresa.get('snk_top_pedido')}
+        dados_cabecalho['CODTIPVENDA'] = {"$":self.dados_empresa.get('snk_codtipvenda')}
+        dados_cabecalho['CODVEND'] = {"$":self.dados_empresa.get('snk_codvend')}
+        dados_cabecalho['DTNEG'] = {"$":data_negociacao}
+        dados_cabecalho['NUNOTA'] = {},
+        dados_cabecalho['TIPMOV'] = {"$":"P"}
+        dados_cabecalho['OBSERVACAO'] = {"$":observacao}
+
+        for item in lista_itens:
+            try:
+                dados_item = {}
+                dados_item['NUNOTA'] = {}
+                dados_item['CODPROD'] = {"$":item.get('codprod')}
+                dados_item['QTDNEG'] = {"$":item.get('quantidade')}
+                dados_item['VLRUNIT'] = {"$":item.get('valor') if item.get('valor') > 0 else 0.01}
+                dados_item['PERCDESC'] = {"$":'0'}
+                dados_item['IGNOREDESCPROMOQTD'] = {"$": "True"}
+                dados_item['CODVOL'] = {"$":item.get('unidade')}
+                dados_item['CODLOCALORIG'] = {"$":self.dados_empresa.get('snk_codlocal_venda')}
+                dados_itens.append(dados_item)
+            except Exception as e:
+                logger.error("Item %s. Erro: %s",item.get('codprod'),e)
+                print(f"Item {item.get('codprod')}. Erro: {e}")
+                continue
 
         return dados_cabecalho, dados_itens
+    
+    @carrega_dados_empresa
+    async def to_sankhya_atualiza_local(
+            self,
+            nunota:int,
+            lista_sequencias:list
+        ) -> list[dict]:
+
+        records:list[dict]=[]        
+        for sequencia in lista_sequencias:
+            try:            
+                records.append({
+                    "pk": {
+                        "NUNOTA": nunota,
+                        "SEQUENCIA": sequencia*-1
+                    },
+                    "values": {
+                        "0": f"{self.dados_empresa.get('snk_codlocal_ecommerce')}"
+                    }
+                })
+            except Exception as e:
+                msg = f"Erro sequencia {sequencia} da nota {nunota}. {e}"
+                logger.error(msg)
+                print(msg)
+            continue
+        return records
+    
+    @carrega_dados_ecommerce
+    @carrega_dados_empresa
+    async def to_sankhya_baixa_estoque_ecommerce(
+            self,
+            lista_itens:list
+        ) -> tuple[dict,list]:
+
+        cabecalho:dict={}
+        try:
+            cabecalho = {
+                "NUNOTA":  {"$": ""},
+                "SERIENOTA":  {"$": "1"},
+                "CODEMP":  {"$": self.dados_empresa.get('snk_codemp_fornecedor')},
+                "CODPARC":  {"$": self.dados_empresa.get('snk_codparc')},
+                "CODNAT":  {"$": "0"},
+                "CODTIPVENDA":  {"$": "11"},
+                "CODTIPOPER":  {"$": self.dados_empresa.get('snk_top_baixa_estoque')},
+                "CODVEND":  {"$": self.dados_empresa.get('snk_codvend')},
+                "CODOBSPADRAO":  {"$": "0"},
+                "DTNEG": {"$": datetime.now().strftime('%d/%m/%Y')},
+                "TIPMOV":  {"$": "V"},
+                "CIF_FOB":  {"$": "C"},
+                "TIPFRETE":  {"$": "N"},
+                "OBSERVACAO":  {"$": "Baixa de estoque das vendas de ecommerce"}
+            }
+
+            itens:list[dict] = []
+            for dados_item in lista_itens:
+                vlrtot = round(dados_item.get('vlrunit') * dados_item.get('qtdneg'),3)
+                item:dict={
+                    "NUNOTA":  {"$": ""},
+                    "SEQUENCIA":  {"$": ""},
+                    "CODPROD":  {"$": dados_item.get('codprod')},
+                    "QTDNEG":  {"$": dados_item.get('qtdneg')},
+                    "VLRUNIT":  {"$":dados_item.get('vlrunit')},
+                    "VLRTOT":  {"$": vlrtot},
+                    "PERCDESC":  {"$": "0"},
+                    "VLRDESC":  {"$": "0"},
+                    "CODVOL":  {"$": dados_item.get('unidade')},
+                    "CODLOCALORIG":  {"$": self.dados_empresa.get('snk_codlocal_ecommerce')},
+                    "CONTROLE":  {"$": dados_item.get('controle') or ''}
+                }
+                itens.append(item)
+                item = {}
+
+        except Exception as e:
+            print(str(e))
+        finally:
+            pass
+
+        return cabecalho, itens
 
     @carrega_dados_ecommerce
     @carrega_dados_empresa
