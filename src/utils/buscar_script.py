3a60a0f261f29c61ecba419e0e629b6377de21a4
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-11-21 11:35:38 -0300

    docstrings

--- src/utils/buscar_script.py
+++ src/utils/buscar_script.py
@@ -4,28 +4,31 @@ from src.utils.load_env import load_env
 load_env()
 logger = set_logger(__name__)
 
-def buscar_script(parametro:str):
-
-    path = os.getenv(parametro)
-    if not path:
-        erro = f"Parâmetro do diretório do script não informado. param: {path}"
-        logger.error(erro)
-        print(erro)
-        return False
-    
+def buscar_script(parametro:str) -> str:
+    """
+    Busca no diretório um script SQL com base na variável de ambiente.
+        :param parametro: nome da variável de ambiente
+    """
+    script:str=''
     try:
-        with open(path, "r") as file:
-            script = " ".join(line.strip() for line in file)
-    except Exception as e:
-        erro = f"Falha ao abrir arquivo do script em {path}. {e}"
-        logger.error(erro)
-        print(erro)
-        return False
+        path:str = os.getenv(parametro)
+        if not path:
+            erro = f"Parâmetro do diretório do script não informado. param: {path}"
+            raise ValueError(erro)
     
-    if not script.strip():
-        erro = f"Arquivo carregado de {path} sem conteúdo"
-        logger.error(erro)
-        print(erro)
-        return False
+        try:
+            with open(path, "r") as file:
+                script = " ".join(line.strip() for line in file)
+        except Exception as e:
+            erro = f"Falha ao abrir arquivo do script em {path}. {e}"
+            raise FileNotFoundError(erro)
+    
+        if not script.strip():
+            erro = f"Arquivo carregado de {path} sem conteúdo"
+            raise ValueError(erro)
+    except Exception as e:
+        logger.error(f"{e}")
+    finally:
+        pass
 
     return script
