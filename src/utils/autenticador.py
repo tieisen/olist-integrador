5e12c70610e7623ffcfcd491f6e2128723795915
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-11-21 11:35:45 -0300

    docstrings

--- src/utils/autenticador.py
+++ src/utils/autenticador.py
@@ -2,28 +2,32 @@ from functools import wraps
 from src.olist.autenticacao import Autenticacao as AuthOlist
 from src.sankhya.autenticacao import Autenticacao as AuthSnk
 
-# BUSCA TOKEN OLIST
 async def buscar_token_olist(self):
     token = await AuthOlist(codemp=self.codemp,empresa_id=self.empresa_id).autenticar()
     self.token = token.strip('"')
 
 def token_olist(func):
+    """
+    Busca o token da API do Olist
+        :param func: função que recebe o decorador
+    """    
     @wraps(func)
     async def wrapper(self, *args, **kwargs):
-        # Garante que os dados da empresa estão carregados
         await buscar_token_olist(self)
         return await func(self, *args, **kwargs)
     return wrapper    
 
-# BUSCA TOKEN SANKHYA
 async def buscar_token_snk(self):
     token = await AuthSnk(codemp=self.codemp,empresa_id=self.empresa_id).autenticar()
     self.token = token.strip('"')
 
 def token_snk(func):
+    """
+    Busca o token da API do Sankhya
+        :param func: função que recebe o decorador
+    """        
     @wraps(func)
     async def wrapper(self, *args, **kwargs):
-        # Garante que os dados da empresa estão carregados
         await buscar_token_snk(self)
         return await func(self, *args, **kwargs)
     return wrapper
