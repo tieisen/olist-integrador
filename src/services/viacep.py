771968b74b35847a65f6a478048683377aaaa484
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-11-21 11:35:50 -0300

    docstrings

--- src/services/viacep.py
+++ src/services/viacep.py
@@ -10,15 +10,23 @@ class Viacep:
     def __init__(self):
         self.regex = r"\D"
 
-    async def busca_ibge_pelo_cep(self,cep:str) -> dict:
-        if not cep:
-            return False
-        cep = re.sub(self.regex,"",cep)
-        if len(cep) != 8:
-            logger.error("CEP inválido: %s",cep)
-            return False
-        res = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
-        if res.status_code == 200:            
-            return int(res.json().get('ibge'))
-        logger.error("CEP %s não encontrado. Erro %s. %s",cep,res.status_code,res.text)
-        return False
+    async def busca_ibge_pelo_cep(self,cep:str) -> int:
+        """
+        Busca o código IBGE do município pelo CEP
+            :param cep: CEP
+        """
+        codigo_ibge:int=None
+
+        try:
+            cep = re.sub(self.regex,"",cep)
+            if len(cep) != 8:
+                raise ValueError(f"CEP inválido: {cep}")
+            
+            res = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
+            codigo_ibge = int(res.json().get('ibge')) if res.ok else 0
+        except Exception as e:
+            codigo_ibge = 0
+            logger.error("Erro ao buscar código IBGE pelo CEP: %s",e)
+        finally:
+            pass
+        return codigo_ibge
