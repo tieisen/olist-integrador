98078dd407d971bf595a9bdaaa3d11a6145279de
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-11-24 10:12:55 -0300

    docstrings

--- src/integrador/nota.py
+++ src/integrador/nota.py
@@ -23,12 +23,13 @@ class Nota:
 
     @contexto
     @carrega_dados_ecommerce
-    async def gerar(
-            self,
-            dados_pedido:dict,
-            **kwargs
-        ) -> dict:
-
+    async def gerar(self,dados_pedido:dict,**kwargs) -> dict:
+        """
+        Gera NF no Olist
+            :param dados_pedido: dicionário com os dados do pedido no Olist
+            :return dict: dicionário com status, dados da NF gerada e erro
+        """
+        
         if not self.log_id:
             self.log_id = await crudLog.criar(empresa_id=self.dados_ecommerce.get('empresa_id'),
                                               de='olist',
@@ -38,40 +39,35 @@ class Nota:
         pedido_olist = PedidoOlist(empresa_id=self.dados_ecommerce.get('empresa_id'))
         try:
             # Gera NF no Olist
-            # print("Gerando NF no Olist...")
             dados_nota_olist = await pedido_olist.gerar_nf(id=dados_pedido.get('id_pedido'))
             if not dados_nota_olist:
                 msg = f"Erro ao gerar NF"
                 raise Exception(msg)
             
             if dados_nota_olist.get('mensagem'):
-                print(dados_nota_olist.get('mensagem'))
                 nota_olist = NotaOlist(id_loja=self.id_loja)
                 dados_nota_olist = await nota_olist.buscar(cod_pedido=dados_pedido.get('cod_pedido'))
-                print(f"#{int(dados_nota_olist.get('numero'))}")                
             
             # Atualiza nota
-            # print("Atualizando status da nota...")
             ack = await crudNota.criar(id_pedido=dados_pedido.get('id_pedido'),
                                        id_nota=dados_nota_olist.get('id'),
                                        numero=int(dados_nota_olist.get('numero')),
                                        serie=str(dados_nota_olist.get('serie')))
             if not ack:
                 msg = f"Erro ao atualizar status da nota"
-                print(msg)
                 raise Exception(msg)            
-            return {"success": True, "dados_nota":dados_nota_olist}
+            return {"success": True, "dados_nota":dados_nota_olist, "__exception__": None}
         except Exception as e:
-            return {"success": False, "__exception__": str(e)}
+            return {"success": False, "dados_nota": None, "__exception__": str(e)}
 
     @contexto
     @carrega_dados_ecommerce
-    async def emitir(
-            self,
-            dados_nota:dict,
-            **kwargs
-        ) -> dict:
-
+    async def emitir(self,dados_nota:dict,**kwargs) -> dict:
+        """
+        Autoriza NF do Olist na Sefaz
+            :param dados_nota: dicionário com os dados da NF
+            :return dict: dicionário com status, chave de acesso da NF autorizada e erro
+        """
         if not self.log_id:
             self.log_id = await crudLog.criar(empresa_id=self.dados_ecommerce.get('empresa_id'),
                                               de='olist',
@@ -81,14 +77,12 @@ class Nota:
         nota_olist = NotaOlist(id_loja=self.id_loja)
         try:
             # Emite a nota
-            # print("Emitindo nota...")
             dados_emissao = await nota_olist.emitir(id=dados_nota.get('id'))
             if not dados_emissao:
                 msg = f"Erro ao emitir nota"
                 raise Exception(msg)
             
             # Atualiza a nota no banco de dados
-            # print("Atualizando status da nota...")
             ack = await crudNota.atualizar(id_nota=dados_nota.get('id'),
                                            chave_acesso=dados_emissao.get('chaveAcesso'),
                                            dh_emissao=datetime.now())
@@ -96,18 +90,17 @@ class Nota:
                 msg = f"Erro ao atualizar status da nota"
                 raise Exception(msg)
                         
-            # print(f"Nota emitida com sucesso!")
-            return {"success": True, "chave_acesso":dados_emissao.get('chaveAcesso')}
+            return {"success": True, "chave_acesso":dados_emissao.get('chaveAcesso'), "__exception__": None}
         except Exception as e:
-            return {"success": False, "__exception__": str(e)}        
+            return {"success": False, "chave_acesso": None, "__exception__": str(e)}        
 
     @carrega_dados_ecommerce
-    async def receber_conta(
-            self,
-            dados_nota:dict,
-            **kwargs
-        ) -> dict:
-
+    async def receber_conta(self,dados_nota:dict,**kwargs) -> dict:
+        """
+        Busca o lançamento de contas a receber referente à NF no Olist
+            :param dados_nota: dicionário com os dados da NF
+            :return dict: dicionário com status, dados do lançamento e erro
+        """
         if not self.log_id:
             self.log_id = await crudLog.criar(empresa_id=self.dados_ecommerce.get('empresa_id'),
                                               de='olist',
@@ -116,7 +109,6 @@ class Nota:
 
         try:            
             # Busca contas a receber no Olist
-            # print("Buscando contas a receber no Olist...")
             nota_olist = NotaOlist(id_loja=self.id_loja)
             dados_financeiro = await nota_olist.buscar_financeiro(serie=str(dados_nota.get('serie')),                                                                  
                                                                   numero=str(dados_nota.get('numero')).zfill(6))
@@ -125,29 +117,27 @@ class Nota:
                 raise Exception(msg)
             
             if dados_financeiro.get('dataLiquidacao'):
-                print(f"Contas a receber da nota já está liquidado")
-            
+                pass
+
             # Atualiza a nota no banco de dados
-            # print("Atualizando status da nota...")
             ack = await crudNota.atualizar(id_nota=dados_nota.get('id'),
                                            id_financeiro=dados_financeiro.get('id'),
                                            dh_baixa_financeiro=dados_financeiro.get('dataLiquidacao',None))
             if not ack:
                 msg = f"Erro ao atualizar contas a receber da nota"
                 raise Exception(msg)            
-            print(f"Contas a receber da nota vinculado com sucesso!")
-            return {"success": True, "dados_financeiro":dados_financeiro}
+            return {"success": True, "dados_financeiro":dados_financeiro, "__exception__": None}
         except Exception as e:
-            return {"success": False, "__exception__": str(e)}
+            return {"success": False, "dados_financeiro": None, "__exception__": str(e)}
 
     @carrega_dados_ecommerce
-    async def baixar_conta(
-            self,
-            id_nota:int,
-            dados_financeiro:dict=None,
-            **kwargs
-        ) -> dict:
-
+    async def baixar_conta(self,id_nota:int,dados_financeiro:dict=None,**kwargs) -> dict:
+        """
+        Faz a baixa do contas a receber referente à NF no Olist
+            :param id_nota: ID da NF no Olist
+            :param dados_financeiro: dicionário com os dados do lançamento do contas a receber
+            :return dict: dicionário com status e erro
+        """
         if not self.log_id:
             self.log_id = await crudLog.criar(empresa_id=self.dados_ecommerce.get('empresa_id'),
                                               de='olist',
@@ -157,56 +147,56 @@ class Nota:
             nota_olist = NotaOlist(id_loja=self.id_loja)
             if not dados_financeiro:
                 # Busca dados do contas a receber no Olist
-                # print("Buscando dados do contas a receber no Olist...")
                 dados_nota = await crudNota.buscar(id_nota=id_nota)
                 if not dados_nota:
                     msg = f"Erro ao buscar dados da nota"
                     raise Exception(msg)                
-                dados_financeiro = await nota_olist.buscar_financeiro(numero=str(dados_nota.get('numero')).zfill(6),
-                                                                      serie=str(dados_nota.get('serie')))
+                dados_financeiro = await nota_olist.buscar_financeiro(numero=str(dados_nota.get('numero')).zfill(6),serie=str(dados_nota.get('serie')))
                 if not dados_financeiro:
                     msg = f"Erro ao buscar contas a receber da nota"
-                    raise Exception(msg)
-            
+                    raise Exception(msg)            
             # Lança recebimento do contas a receber
-            # print("Lançando baixa do contas a receber...")
-            ack = await nota_olist.baixar_financeiro(id=dados_financeiro.get('id'),
-                                                     valor=dados_financeiro.get('valor'))
+            ack = await nota_olist.baixar_financeiro(id=dados_financeiro.get('id'),valor=dados_financeiro.get('valor'))
             if not ack:
                 msg = f"Erro ao baixar contas a receber da nota"
-                raise Exception(msg)
-            
+                raise Exception(msg)            
             # Atualiza a nota no banco de dados
-            ack = await crudNota.atualizar(id_nota=id_nota,
-                                           dh_baixa_financeiro=datetime.now())
+            ack = await crudNota.atualizar(id_nota=id_nota,dh_baixa_financeiro=datetime.now())
             if not ack:
                 msg = f"Erro ao atualizar contas a receber da nota"
                 raise Exception(msg)            
-            print(f"Contas a receber da nota vinculado com sucesso!")
-            return {"success": True}
+            return {"success": True, "__exception__": None}
         except Exception as e:
             return {"success": False, "__exception__": str(e)}
     
     async def registrar_cancelamento(self,dados_nota:dict) -> dict:
+        """
+        Regitra o cancelamento de uma NF            
+            :param dados_nota: dicionário com os dados da NF
+            :return dict: dicionário com status e erro
+        """
         try:
             # Atualiza a nota no banco de dados
-            print("Atualizando status da nota...")
-            ack = await crudNota.atualizar(id_nota=dados_nota.get('id'),
-                                           dh_cancelamento=datetime.now()) 
+            ack = await crudNota.atualizar(id_nota=dados_nota.get('id'),dh_cancelamento=datetime.now()) 
             if not ack:
                 msg = f"Erro ao atualizar status da nota"
                 raise Exception(msg)                        
-            print(f"Nota cancelada com sucesso!")
-            return {"success": True}
+            return {"success": True, "__exception__": None}
         except Exception as e:
             return {"success": False, "__exception__": str(e)}
     
     @carrega_dados_ecommerce
     async def cancelar(self,id:int=None,chave:str=None,numero:int=None) -> dict:
+        """
+        Exclui uma nota de venda no Sankhya. Somente para importação por pedido.
+            :param id: ID da NF no Olist
+            :param chave: chave de acesso da NF
+            :param numero: número da NF
+            :return dict: dicionário com status, dados da nota e erro
+        """        
         nota_snk = NotaSnk(empresa_id=self.dados_ecommerce.get('empresa_id'))
         try:
             # Busca nota
-            print("Buscando nota...")
             numero_ecommerce:dict={}
             if numero:
                 numero_ecommerce = {
@@ -220,27 +210,25 @@ class Nota:
                 msg = f"Nota não encontrada"
                 raise Exception(msg)            
             # Excluir nota
-            print(f"Excluindo nota...")
             ack = await nota_snk.excluir(nunota=dados_nota.get('nunota'))
             if not ack:
                 msg = "Erro ao excluir nota."
                 raise Exception(msg)                        
-            print(f"Nota excluída com sucesso!")
-            return {"success": True, "dados_nota":dados_nota}
+            return {"success": True, "dados_nota":dados_nota, "__exception__": None}
         except Exception as e:
-            return {"success": False, "__exception__": str(e)}
+            return {"success": False, "dados_nota":None, "__exception__": str(e)}
 
     @contexto
     @log_execucao
     @carrega_dados_ecommerce
-    async def integrar_cancelamento(
-            self,
-            id:int=None,
-            chave:str=None,
-            numero:int=None,
-            **kwargs
-        ) -> bool:
-
+    async def integrar_cancelamento(self,id:int=None,chave:str=None,numero:int=None,**kwargs) -> bool:
+        """
+        Rotina para excluir uma nota de venda no Sankhya. Somente para importação por pedido.
+            :param id: ID da NF no Olist
+            :param chave: chave de acesso da NF
+            :param numero: número da NF
+            :return bool: status da operação
+        """ 
         self.log_id = await crudLog.criar(empresa_id=self.dados_ecommerce.get('empresa_id'),
                                           de='olist',
                                           para='sankhya',
