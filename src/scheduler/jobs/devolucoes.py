2499712247fe302d513932058a95f3e275e97ffc
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-11-24 10:13:31 -0300

    jobs.devolucoes: bug fix na execucao de funcao async

--- src/scheduler/jobs/devolucoes.py
+++ src/scheduler/jobs/devolucoes.py
@@ -15,7 +15,7 @@ async def integrar_devolucoes(codemp:int=None):
 
     for i, emp in enumerate(empresas):
         print(f"\nEmpresa {emp.get('nome')} ({i+1}/{len(empresas)})".upper())
-        ecommerces = asyncio.run(ecommerce.buscar(empresa_id=emp.get('id')))
+        ecommerces = await ecommerce.buscar(empresa_id=emp.get('id'))
         for j, ecom in ecommerces:
             print(f"E-commerce {ecom.get('nome')} ({j+1}/{len(ecom)})".upper())
             devolucao = Devolucao(id_loja=ecom.get('id_loja'))
