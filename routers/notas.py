040d30b7b4f4c59b7788082b5a321286e9e00eef
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-12-01 17:32:38 -0300

    routers.notas: baixa de titulos movida

--- routers/notas.py
+++ routers/notas.py
@@ -9,24 +9,10 @@ class NotaModel(BaseModel):
     id_loja:int
     numero:int
 
-class ContaModel(BaseModel):
-    id_loja:int
-    id:int
-
-@router.post("/baixar-contas")
-async def baixar_contas(nota:ContaModel) -> bool:
-    """
-    Baixa contas a receber que estão pendentes.
-    """
-    _nota = Nota(id_loja=nota.id_loja)
-    if not asyncio.run(_nota.baixar_conta(id_nota=nota.id)):
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao baixar contas a receber")
-    return True
-
 @router.post("/cancelar")
 async def cancelar_nota(nota:NotaModel) -> bool:
     """
-    Cancela nota de devolução
+    Cancela nota
     """
     _nota = Nota(id_loja=nota.id_loja)
     if not asyncio.run(_nota.integrar_cancelamento(numero=nota.numero)):
