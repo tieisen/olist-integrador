3632ad9125db3af0004e46ee907d57698c7b61a2
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-12-01 17:56:45 -0300

    routers.financeiro: validacao do formato da variavel de data

--- routers/financeiro.py
+++ routers/financeiro.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
 from src.integrador.financeiro import Financeiro
+from datetime import datetime
 import asyncio
 from pydantic import BaseModel
 
@@ -14,6 +15,10 @@ async def baixar_contas(financeiro:FinanceiroModel) -> bool:
     """
     Baixa contas a receber com base no relatório de custos do e-commerce.
     """
+    try:
+        financeiro.data = datetime.strptime(financeiro.data, "%Y-%m-%d").date()
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Formato de data inválido. Use 'YYYY-MM-DD'.")
     fin = Financeiro(id_loja=financeiro.id_loja)
     if not asyncio.run(fin.executar_baixa(data=financeiro.data)):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao baixar contas a receber")
