e775a9204169c69c4e6d70bd23761458585daabc
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-12-02 18:00:28 -0300

    routers.financeiro: dispara job no lugar da rotina de integracao

--- routers/financeiro.py
+++ routers/financeiro.py
@@ -1,13 +1,12 @@
 from fastapi import APIRouter, HTTPException, status
-from src.integrador.financeiro import Financeiro
+from src.scheduler.jobs.financeiro import integrar_financeiro
 from datetime import datetime
-import asyncio
 from pydantic import BaseModel
 
 router = APIRouter()
 
 class FinanceiroModel(BaseModel):
-    id_loja:int
+    codemp:int
     data:str
 
 @router.post("/baixar")
@@ -16,10 +15,9 @@ async def baixar_contas(financeiro:FinanceiroModel) -> bool:
     Baixa contas a receber com base no relatório de custos do e-commerce.
     """
     try:
-        financeiro.data = datetime.strptime(financeiro.data, "%Y-%m-%d").date()
+        financeiro.data = datetime.strptime(financeiro.data, "%Y-%m-%d")
     except ValueError:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Formato de data inválido. Use 'YYYY-MM-DD'.")
-    fin = Financeiro(id_loja=financeiro.id_loja)
-    if not asyncio.run(fin.executar_baixa(data=financeiro.data)):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Formato de data inválido. Use 'YYYY-MM-DD'. Data informada: {financeiro.data}")
+    if not await integrar_financeiro(data=financeiro.data,codemp=financeiro.codemp):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erro ao baixar contas a receber")
     return True
