777a0ab6488078d92672dfdddd660b43b957c98b
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-12-05 15:42:47 -0300

    jobs.faturamento: adicionado codemp na instancia de faturamento

--- src/scheduler/jobs/faturamento.py
+++ src/scheduler/jobs/faturamento.py
@@ -19,7 +19,7 @@ async def integrar_faturamento(codemp:int=None, id_loja:int=None) -> dict:
                 ecommerces = await ecommerce.buscar(empresa_id=emp.get('id'))
                 for j, ecom in enumerate(ecommerces):
                     print(f"E-commerce {ecom.get('nome')} ({j+1}/{len(ecommerces)})".upper())
-                    faturamento = Faturamento(id_loja=ecom.get('id_loja'))
+                    faturamento = Faturamento(id_loja=ecom.get('id_loja'),codemp=emp.get('snk_codemp'))
                     await faturamento.integrar_olist()
                     await faturamento.integrar_snk()
             retorno = {
