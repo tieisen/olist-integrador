82cb49ad9fa87eb00c4256543a750e5ed31f75ac
TI <ti@grupoeisen.com.br>
2025-12-05 15:11:50 -0300

    jobs.devolucoes: corrigido execucao async

--- src/scheduler/jobs/devolucoes.py
+++ src/scheduler/jobs/devolucoes.py
@@ -17,8 +17,8 @@ async def integrar_devolucoes(codemp:int=None):
 
     for i, emp in enumerate(empresas):
         print(f"\nEmpresa {emp.get('nome')} ({i+1}/{len(empresas)})".upper())
-        ecommerces = asyncio.run(ecommerce.buscar(empresa_id=emp.get('id')))
-        for j, ecom in ecommerces:
+        ecommerces = await ecommerce.buscar(empresa_id=emp.get('id'))
+        for j, ecom in enumerate(ecommerces):
             print(f"E-commerce {ecom.get('nome')} ({j+1}/{len(ecom)})".upper())
             devolucao = Devolucao(id_loja=ecom.get('id_loja'))
             ack.append(await devolucao.integrar_receber())
