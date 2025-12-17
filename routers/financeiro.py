eec3bdf000bc232d5ed657eb1cf26d9f693ce8a1
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-12-17 15:16:56 -0300

    routers.financeiro: adicionada rota para baixa de pedidos shopee

--- routers/financeiro.py
+++ routers/financeiro.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, status
-from src.scheduler.jobs.financeiro import integrar_financeiro
+from src.scheduler.jobs.financeiro import integrar_financeiro, baixar_shopee
 from datetime import datetime
 from pydantic import BaseModel
 
@@ -8,6 +8,40 @@ router = APIRouter()
 class FinanceiroModel(BaseModel):
     codemp:int
     data:str
+
+class RecebimentoShopeeModel(BaseModel):
+    data:str
+    tipo_de_transacao:str=None
+    descricao:str=None
+    id_do_pedido:str
+    direcao_do_dinheiro:str=None
+    valor:float
+    status:str=None
+    balanca_apos_as_transacoes:float=None
+    valor_a_ser_ajustado:float=None
+
+class FinanceiroShopeeModel(BaseModel):
+    codemp:int
+    dados:list[RecebimentoShopeeModel]    
+
+@router.post("/baixar/shopee")
+async def baixar_titulos_shopee(financeiro:FinanceiroShopeeModel) -> bool:    
+    """
+    Baixa títulos pendentes dos pedidos da Shopee.
+    """
+
+    try:
+        codemp:int = financeiro.codemp
+        data:list[dict] = financeiro.model_dump()
+        if not data:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Dados inválidos")
+        data = data.get('dados')
+        res = await baixar_shopee(codemp=codemp,data=data)
+        if not res.get('status'):
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail=res.get('exception'))
+        return res.get('status')
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail=str(e))
 
 @router.post("/baixar")
 async def baixar_contas(financeiro:FinanceiroModel) -> bool:
