54ea11b1b3def36d8047d178df8a2b9a3a2cdea7
TI <ti@grupoeisen.com.br>
2025-12-19 15:23:16 -0300

    utils.load_env: update

--- src/utils/load_env.py
+++ src/utils/load_env.py
@@ -2,4 +2,4 @@ from dotenv import load_dotenv
 
 def load_env() -> load_dotenv:
     """ Carrega o arquivo com as variáveis de ambiente """
-    return load_dotenv('keys/.test.env')
+    return load_dotenv('keys/.env')
