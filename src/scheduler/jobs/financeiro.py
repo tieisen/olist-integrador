97d86b57276b1354edd38ce41f500fe2da570bf7
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-12-23 10:32:24 -0300

    jobs.financeiro: adicionada rotina integrar_recebimentos_shopee

--- src/scheduler/jobs/financeiro.py
+++ src/scheduler/jobs/financeiro.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from database.crud import empresa, ecommerce
+from database.crud import empresa, ecommerce, shopee
 from src.integrador.financeiro import Financeiro
 from src.services.bot import Bot
 
@@ -73,6 +73,41 @@ async def integrar_financeiro(data:str,codemp:int=None) -> dict:
                 }
             else:
                 raise Exception("Erro ao baixar relatório de custos")
+    except Exception as e:
+        print(f"{e}")
+        retorno = {
+            "status": False,
+            "exception": f"{e}"
+        }
+    finally:
+        return retorno
+
+async def integrar_recebimentos_shopee(codemp:int=None) -> dict:
+
+    retorno:dict={}
+    empresas:list[dict]=[]
+    emp:dict={}
+
+    print(":::::::::::::::::::: BUSCANDO RECEBIMENTOS SHOPEE ::::::::::::::::::::")    
+
+    empresas = await empresa.buscar(codemp=codemp)
+    try:
+        for i, emp in enumerate(empresas):
+            print(f"\nEmpresa {emp.get('nome')} ({i+1}/{len(empresas)})".upper())
+            loja_shopee = await shopee.buscar(empresa_id=emp.get('id'))
+            if not loja_shopee:                
+                print(f"Loja Shopee não encontrada para esta empresa.")
+                continue
+            id_loja = await shopee.buscar_idloja(ecommerce_id=loja_shopee.get('ecommerce_id'))
+            financeiro = Financeiro(empresa_id=emp.get('id'),id_loja=id_loja)
+            print("\nBuscando contas a receber...")
+            print(await financeiro.buscar_contas_shopee(ecommerce_id=loja_shopee.get('ecommerce_id')))
+            print("\nBaixando contas a receber...")
+            print(await financeiro.baixar_contas_receber_shopee())
+            retorno = {
+                "status": True,
+                "exception": None
+            }
     except Exception as e:
         print(f"{e}")
         retorno = {
