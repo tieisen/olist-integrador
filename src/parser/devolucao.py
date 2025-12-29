f4add96fae2eda22ed7d826f77612db3be870a06
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2025-12-29 14:16:13 -0300

    parser.devolucao: adicionado to_sankhya_

--- src/parser/devolucao.py
+++ src/parser/devolucao.py
@@ -1,5 +1,6 @@
 from src.utils.log import set_logger
 from src.utils.load_env import load_env
+from datetime import datetime
 load_env()
 logger = set_logger(__name__)
 
@@ -48,3 +49,51 @@ class Devolucao:
                 continue
 
         return resultado
+           
+    def to_sankhya_(self,dados_empresa:dict,dados_nfd:dict) -> list[dict]:
+        """
+        Converte os dados da NFD no formato da API do Sankhya.
+            :param dados_empresa: dicionário com os dados do cabeçalho da nota
+            :param dados_nfd:lista de itens dos pedidos da API do Olist
+            :return dict: dicionário com os dados do cabeçalho da nota
+            :return list[dict]: lista de dicionários com as dados dos itens da nota
+        """
+
+        dados_cabecalho = {}
+        dados_itens = []
+
+        data_negociacao = datetime.strptime(dados_nfd.get('dataEmissao'),'%Y-%m-%d').strftime('%d/%m/%Y')
+        observacao = f"NFD {dados_nfd.get('numero')} de {data_negociacao}\n"+dados_nfd.get('observacoes')
+        
+        dados_cabecalho['CIF_FOB'] = {"$":'C'}
+        dados_cabecalho['CODCENCUS'] = {"$":'0'}
+        dados_cabecalho['CODEMP'] = {"$":dados_empresa.get('snk_codemp_fornecedor')}
+        dados_cabecalho['CODNAT'] = {"$":dados_empresa.get('snk_codnat')}
+        dados_cabecalho['CODPARC'] = {"$":dados_empresa.get('snk_codparc')}
+        dados_cabecalho['CODTIPOPER'] = {"$":dados_empresa.get('snk_top_devolucao')}
+        dados_cabecalho['CODTIPVENDA'] = {"$":dados_empresa.get('snk_codtipvenda')}
+        dados_cabecalho['CODVEND'] = {"$":dados_empresa.get('snk_codvend')}
+        dados_cabecalho['DTNEG'] = {"$":data_negociacao}
+        dados_cabecalho['NUNOTA'] = {},
+        dados_cabecalho['NUMNOTA'] = {"$":int(dados_nfd.get('numero'))},
+        dados_cabecalho['SERIENOTA'] = {"$":"2"},
+        dados_cabecalho['TIPMOV'] = {"$":"D"}
+        dados_cabecalho['OBSERVACAO'] = {"$":observacao}
+
+        for item in dados_nfd.get('itens'):
+            try:
+                dados_item = {}
+                dados_item['NUNOTA'] = {}
+                dados_item['CODPROD'] = {"$":item.get('codigo')}
+                dados_item['QTDNEG'] = {"$":item.get('quantidade')}
+                dados_item['VLRUNIT'] = {"$":item.get('valorUnitario')}
+                dados_item['PERCDESC'] = {"$":'0'}
+                dados_item['IGNOREDESCPROMOQTD'] = {"$": "True"}
+                dados_item['CODVOL'] = {"$":item.get('unidade')}
+                dados_item['CODLOCALORIG'] = {"$":dados_empresa.get('snk_codlocal_ecommerce')}
+                dados_itens.append(dados_item)
+            except Exception as e:
+                logger.error("Item %s. Erro: %s",item.get('codigo'),e)
+                continue
+
+        return dados_cabecalho, dados_itens
