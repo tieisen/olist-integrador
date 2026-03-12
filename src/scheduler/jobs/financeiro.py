0839c8f50577756c78075afeb001a749da89b6a1
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2026-03-12 09:23:19 -0300

    jobs.financeiro: rotinas consultarRecebimentosShopee e integrar lancando vlr liq e taxa

--- src/scheduler/jobs/financeiro.py
+++ src/scheduler/jobs/financeiro.py
@@ -1,90 +1,82 @@
-from datetime import datetime
-from database.crud import empresa, ecommerce, shopee
-from src.integrador.financeiro import Financeiro
-from src.services.bot import Bot
+from database.crud import empresa, ecommerce, shopee, nota
+from src.olist.nota import Nota as NotaOlist
+from src.integrador.financeiro import Receita, Despesa
+import time
 
-async def baixar_shopee(codemp:int,data:dict) -> dict:
+async def integrar(codemp:int|None=None,dtFim:str|None=None) -> dict:
 
-    retorno:dict={}
+    retorno:dict={
+            "status": False,
+            "exception": ""
+        }
     empresas:list[dict]=[]
     ecommerces:list[dict]=[]
     emp:dict={}
     ecom:dict={}
-
-    print(":::::::::::::::::::: BAIXA DE TÍTULOS A RECEBER ::::::::::::::::::::")    
+    lista_notas_emitidas:list[dict]=[]
+    lista_nota_lcto:list[dict]=[]
 
     empresas = await empresa.buscar(codemp=codemp)
     try:
         for i, emp in enumerate(empresas):
             print(f"\nEmpresa {emp.get('nome')} ({i+1}/{len(empresas)})".upper())
+            receita = Receita(empresaId=emp.get('id'))
+            despesa = Despesa(empresaId=emp.get('id'))            
+            notas = NotaOlist(empresa_id=emp.get('id'))
+            
+            print("Buscando notas emitidas...")
+            lista_notas_emitidas = await notas.buscarData(data=dtFim)
+            print(f"Notas emitidas: {len(lista_notas_emitidas)}")
+            
+            print("Processando notas emitidas...")
+            await receita.processarNotas(listaNotas=lista_notas_emitidas)
+
             ecommerces = await ecommerce.buscar(empresa_id=emp.get('id'))
             for j, ecom in enumerate(ecommerces):
-                if 'SHOPEE' in ecom.get('nome').upper():
-                    financeiro = Financeiro(id_loja=ecom.get('id_loja'),empresa_id=ecom.get('empresa_id'))
-                    ack = await financeiro.baixar_contas_receber_shopee(relatorio_recebimentos=data)
-
-        retorno = {
-            "status": ack,
-            "exception": "Ocorreu um erro ao baixar contas a receber. Verifique as contas pendentes no Olist" if not ack else None                    
-        }                
-    except Exception as e:
-        print(f"{e}")
-        retorno = {
-            "status": False,
-            "exception": f"{e}"
-        }
-    finally:
-        return retorno
-
-async def integrar_financeiro(data:str,codemp:int=None) -> dict:
-
-    retorno:dict={}
-    empresas:list[dict]=[]
-    ecommerces:list[dict]=[]
-    emp:dict={}
-    ecom:dict={}
-    lista_status:list[bool]=[]
-
-    if not isinstance(data,datetime):
-        try:
-            data = datetime.strptime(data, "%Y-%m-%d")
-        except:
-            raise Exception(f"Data inválida. Use o formato YYYY-MM-DD. Data informada: {data}")
-
-
-    print(":::::::::::::::::::: CONTAS A RECEBER ::::::::::::::::::::")    
-
-    empresas = await empresa.buscar(codemp=codemp)
-    try:
-        for i, emp in enumerate(empresas):
-            print(f"\nEmpresa {emp.get('nome')} ({i+1}/{len(empresas)})".upper())
-            bot = Bot(empresa_id=emp.get('id'))
-            if await bot.rotina_relatorio_custos(data=data):            
-                ecommerces = await ecommerce.buscar(empresa_id=emp.get('id'))
-                for j, ecom in enumerate(ecommerces):
-                    if 'SHOPEE' in ecom.get('nome').upper():
+                print(f"\nE-commerce {ecom.get('nome')} ({j+1}/{len(ecommerces)})".upper())
+                receita.dados_ecommerce = None
+                receita.id_loja = ecom.get('id_loja')
+                despesa.dados_ecommerce = None                
+                despesa.id_loja = ecom.get('id_loja')             
+                    
+                print(f"Buscando contas pendentes...")
+                lista_nota_lcto = await nota.buscarPendenteLcto(ecommerce_id=ecom.get('id'))
+                if not lista_nota_lcto:
+                    continue
+                
+                for n, nota_lcto in enumerate(lista_nota_lcto):                    
+                    if ('SHOPEE' in ecom.get('nome').upper()) and (nota_lcto['income_data'].get('fee_shopee',0)==0):
                         continue
-                    print(f"E-commerce {ecom.get('nome')} ({j+1}/{len(ecommerces)})".upper())
-                    financeiro = Financeiro(id_loja=ecom.get('id_loja'),empresa_id=ecom.get('empresa_id'))
-                    # Agrupa títulos dos pedidos parcelados
-                    await financeiro.agrupar_titulos_parcelados()                    
-                    lista_status.append(await financeiro.executar_baixa(data=data))
-                retorno = {
-                    "status": all(lista_status),
-                    "exception": "Erro ao baixar contas a receber" if not all(lista_status) else None
-                }
-            else:
-                raise Exception("Erro ao baixar relatório de custos")
-    except Exception as e:
-        print(f"{e}")
-        retorno = {
-            "status": False,
-            "exception": f"{e}"
-        }
+                    time.sleep(receita.req_time_sleep)
+                    print(f"->> Nota {nota_lcto.get('numero')}/{nota_lcto.get('id_nota')}: ({n+1}/{len(lista_nota_lcto)})".upper())
+                    if not nota_lcto.get('id_financeiro'):
+                        print(f"Formatando payload receita...")
+                        await receita.formatarPayloadLcto(dadosConta=nota_lcto)
+                        print(f"Lançando receita...")
+                        await receita.lancarConta()
+                        print(f"RECEITA OK!!")
+                    
+                    if len(str(ecom.get('id_loja'))) >= 9:
+                        # Funcionários ou Parfum
+                        await despesa.ignorarTaxa(id_nota=nota_lcto.get('id'))
+                        continue
+                    
+                    if not nota_lcto.get('id_financeiro_taxa'):
+                        print(f"Formatando payload despesa...")
+                        await despesa.formatarPayloadLcto(dadosConta=nota_lcto)
+                        print(f"Lançando despesa...")                    
+                        await despesa.lancarConta()
+                        print(f"DESPESA OK!!")
+        
+        retorno['status'] = True        
+    except Exception as e:        
+        retorno['exception'] = str(e)
     finally:
-        return retorno
+        pass
+    
+    return retorno
 
-async def integrar_recebimentos_shopee(codemp:int=None) -> dict:
+async def consultarRecebimentosShopee(codemp:int=None) -> dict:
 
     retorno:dict={}
     empresas:list[dict]=[]
@@ -96,16 +88,15 @@ async def integrar_recebimentos_shopee(codemp:int=None) -> dict:
     try:
         for i, emp in enumerate(empresas):
             print(f"\nEmpresa {emp.get('nome')} ({i+1}/{len(empresas)})".upper())
-            loja_shopee = await shopee.buscar(empresa_id=emp.get('id'))
-            if not loja_shopee:                
-                print(f"Loja Shopee não encontrada para esta empresa.")
-                continue
-            id_loja = await shopee.buscar_idloja(ecommerce_id=loja_shopee.get('ecommerce_id'))
-            financeiro = Financeiro(empresa_id=emp.get('id'),id_loja=id_loja)
-            print("\nBuscando contas a receber...")
-            print(await financeiro.buscar_contas_shopee(ecommerce_id=loja_shopee.get('ecommerce_id')))
-            print("\nBaixando contas a receber...")
-            print(await financeiro.baixar_contas_receber_shopee())
+            receita = Receita(empresaId=emp.get('id'))            
+            
+            ecommerces = await ecommerce.buscar(empresa_id=emp.get('id'))
+            for j, ecom in enumerate(ecommerces):
+                if 'SHOPEE' in ecom.get('nome').upper():
+                    loja_shopee = await shopee.buscar(ecommerce_id=ecom.get('id'))
+                    if loja_shopee:
+                        print("Buscando contas do Shopee...")
+                        await receita.buscarContasShopee(ecommerceId=ecom.get('id'))
             retorno = {
                 "status": True,
                 "exception": None
