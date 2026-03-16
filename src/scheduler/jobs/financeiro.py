9550ee03f11f8c749b3ee133bf92ead1d1725bf5
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2026-03-16 15:32:45 -0300

    scheduler.financeiro: logs

--- src/scheduler/jobs/financeiro.py
+++ src/scheduler/jobs/financeiro.py
@@ -2,6 +2,8 @@ from database.crud import empresa, ecommerce, shopee, nota
 from src.olist.nota import Nota as NotaOlist
 from src.integrador.financeiro import Receita, Despesa
 import time
+from src.utils.log import set_logger
+logger = set_logger(__name__)
 
 async def integrar(codemp:int|None=None,dtFim:str|None=None) -> dict:
 
@@ -20,6 +22,7 @@ async def integrar(codemp:int|None=None,dtFim:str|None=None) -> dict:
     try:
         for i, emp in enumerate(empresas):
             print(f"\nEmpresa {emp.get('nome')} ({i+1}/{len(empresas)})".upper())
+            logger.info(f"\nEmpresa {emp.get('nome')} ({i+1}/{len(empresas)})".upper())
             receita = Receita(empresaId=emp.get('id'))
             despesa = Despesa(empresaId=emp.get('id'))            
             notas = NotaOlist(empresa_id=emp.get('id'))
@@ -29,27 +32,34 @@ async def integrar(codemp:int|None=None,dtFim:str|None=None) -> dict:
             print(f"Notas emitidas: {len(lista_notas_emitidas)}")
             
             print("Processando notas emitidas...")
+            logger.info("Processando notas emitidas...")
             await receita.processarNotas(listaNotas=lista_notas_emitidas)
 
             ecommerces = await ecommerce.buscar(empresa_id=emp.get('id'))
             for j, ecom in enumerate(ecommerces):
                 print(f"\nE-commerce {ecom.get('nome')} ({j+1}/{len(ecommerces)})".upper())
+                logger.info(f"\nE-commerce {ecom.get('nome')} ({j+1}/{len(ecommerces)})".upper())
                 receita.dados_ecommerce = None
                 receita.id_loja = ecom.get('id_loja')
                 despesa.dados_ecommerce = None                
                 despesa.id_loja = ecom.get('id_loja')  
 
                 if ('SHOPEE' in ecom.get('nome').upper()):
+                    logger.info("Consultando recebimentos Shopee...")
                     await consultarRecebimentosShopee(codemp=emp.get('id'))
 
                 print(f"Buscando contas pendentes...")
-                lista_nota_lcto = await nota.buscarPendenteLcto(ecommerce_id=ecom.get('id'))
+                logger.info(f"Buscando contas pendentes...")
+                lista_nota_lcto = await nota.buscarPendenteLcto(ecommerce_id=ecom.get('id'))                
+                logger.info(f"Contas pendentes: {len(lista_nota_lcto)}")
                 if not lista_nota_lcto:
                     continue
                 
                 for n, nota_lcto in enumerate(lista_nota_lcto):                    
-                    if ('SHOPEE' in ecom.get('nome').upper()) and (nota_lcto['income_data'].get('fee_shopee',0)==0):
-                        continue
+                    if ('SHOPEE' in ecom.get('nome').upper()):
+                        if nota_lcto['income_data'].get('fee_shopee',0)==0:
+                            continue
+                        
                     time.sleep(receita.req_time_sleep)
                     print(f"->> Nota {nota_lcto.get('numero')}/{nota_lcto.get('id_nota')}: ({n+1}/{len(lista_nota_lcto)})".upper())
                     if not nota_lcto.get('id_financeiro'):
